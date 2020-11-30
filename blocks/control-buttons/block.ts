e2d5343f5a2d4f1239822e423a45ed84234f2157
plosys <89532245+plosys@users.noreply.github.com>
2020-11-30 18:55:53 +0000

    Version bump to 0.9.2

--- blocks/control-buttons/block.ts
+++ blocks/control-buttons/block.ts
@@ -4,4 +4,53 @@ export interface Button {
   /**
    * A [Font Awesome icon](https://fontawesome.com/icons?m=free) name to render on the button.
    *
-   * By default a cared pointing left or right
+   * By default a cared pointing left or right respectively will be rendered.
+   */
+  icon?: IconName;
+}
+
+declare module '@appsemble/sdk' {
+  interface Parameters {
+    /**
+     * The configuration for the back button.
+     *
+     * Set to `false` to hide back button.
+     */
+    back?: Button | false;
+
+    /**
+     * The configuration for the forward button.
+     *
+     * Set to `false` to hide forward button.
+     */
+    forward?: Button | false;
+  }
+
+  interface Actions {
+    /**
+     * This action gets triggered then the back button is clicked.
+     *
+     * It will be called with the page data.
+     */
+    onBack: never;
+
+    /**
+     * This action gets triggered then the back button is clicked.
+     *
+     * It will be called with the page data.
+     */
+    onForward: never;
+  }
+
+  interface Messages {
+    /**
+     * The label that’s used on the back button.
+     */
+    back: never;
+
+    /**
+     * The label that’s used on the forward button.
+     */
+    forward: never;
+  }
+}
---
+++ blocks/data-loader/block.ts
@@ -0,0 +1,38 @@
+declare module '@appsemble/sdk' {
+  interface Actions {
+    /**
+     * Action that gets dispatched when a new filter gets applied.
+     *
+     * This also gets called during the initial load.
+     */
+    onLoad: never;
+  }
+
+  interface Messages {
+    /**
+     * The error message that should be shown when data failed to load.
+     */
+    loadErrorMessage: never;
+  }
+
+  interface Parameters {
+    /**
+     * By default the `onLoad` action is triggered immediately.
+     *
+     * By setting this to `true`, this won’t happen.
+     */
+    skipInitialLoad?: boolean;
+  }
+
+  interface EventEmitters {
+    /**
+     * Event that gets emitted once the `onLoad` action has finished.
+     *
+     * It can be triggered again by sending a `refresh` event.
+     */
+    data: never;
+  }
+
+  interface EventListeners {
+    /**
+     * When received, the `onLoad` 
