5a741bf65008c354ede7832bdd9fd44a7bca2660
plosys <89532245+plosys@users.noreply.github.com>
2020-12-20 06:45:59 +0000

    change user

--- blocks/table/block.ts
+++ blocks/table/block.ts
@@ -35,4 +35,185 @@ export interface Field extends BaseField {
 export interface Button extends BaseField {
   /**
    * The name of the action to trigger when clicking on this field.
-  
+   *
+   * @format action
+   */
+  onClick?: string;
+
+  /**
+   * The definition of the contents and styling of the button.
+   */
+  button: {
+    /**
+     * The color of the button.
+     *
+     * @default "primary"
+     */
+    color?: BulmaColor;
+
+    /**
+     * Whether the button should be disabled.
+     *
+     * If the resulting remapper value is truthy, the button will be disabled.
+     */
+    disabled?: Remapper;
+
+    /**
+     * The size of the button.
+     *
+     * @default "normal"
+     */
+    size?: BulmaSize;
+
+    /**
+     * The label to display inside of the button.
+     */
+    label?: Remapper;
+
+    /**
+     * An optional FontAwesome icon to display inside of the button.
+     */
+    icon?: IconName;
+
+    /**
+     * When set to true, the ‘light’ set of Bulma colors are used.
+     */
+    light?: boolean;
+
+    /**
+     * Whether the button should be rounded.
+     */
+    rounded?: boolean;
+
+    /**
+     * Whether the button should be full width or not.
+     *
+     * By default buttons only take up as much space as needed.
+     */
+    fullwidth?: boolean;
+
+    /**
+     * Whether the text and background colors should be inverted.
+     */
+    inverted?: boolean;
+
+    /**
+     * Whether the button should display its colors in the outlines.
+     */
+    outlined?: boolean;
+
+    /**
+     * The title for the button.
+     *
+     * Describe what the button does. This helps with accessibility for people using screen readers.
+     */
+    title?: Remapper;
+  };
+}
+
+export interface Dropdown extends BaseField {
+  dropdown: {
+    /**
+     * The text to show in the dropdown button.
+     */
+    label?: Remapper;
+
+    /**
+     * The icon to show in the dropdown button.
+     */
+    icon?: IconName;
+
+    /**
+     * The list of options to display. Must have at least 1 option.
+     *
+     * @minItems 1
+     */
+    options: DropdownOption[];
+
+    /**
+     * How the dropdown should be aligned.
+     *
+     * @default 'right'
+     */
+    alignment?: 'left' | 'right';
+  };
+}
+
+export interface DropdownOption {
+  /**
+   * The text to show in the option.
+   */
+  label?: Remapper;
+
+  /**
+   * The icon to show in the option.
+   */
+  icon?: IconName;
+
+  /**
+   * The action that will be called when selecting this option.
+   *
+   * @format action
+   */
+  onClick: string;
+}
+
+/**
+ * Repeated fields based on an input array.
+ *
+ * Only one repeated field is allowed, only the first RepeatedField is considered.
+ */
+export interface RepeatedField {
+  /**
+   * The list of fields that should be repeated for each array item.
+   */
+  repeat: (Button | Dropdown | Field)[];
+
+  /**
+   * The value to use as the base of the repeated field.
+   *
+   * Should be an array of data.
+   */
+  value: Remapper;
+}
+
+declare module '@appsemble/sdk' {
+  interface Messages {
+    /**
+     * The message to display when data could not be loaded.
+     */
+    error: never;
+
+    /**
+     * The message to display when the data to display is empty.
+     */
+    emptyMessage: never;
+  }
+  interface Parameters {
+    /**
+     * A list of fields to display.
+     */
+    fields: (Button | Dropdown | Field | RepeatedField)[];
+  }
+
+  interface Actions {
+    /**
+     * The default action that is triggered when clicking on a row.
+     *
+     * This does not trigger if the field has a different action specified.
+     */
+    onClick: never;
+
+    /**
+     * Custom action mapping.
+     */
+    [key: string]: never;
+  }
+
+  interface EventListeners {
+    /**
+     * Listener for data used to display data in the table.
+     */
+    data: never;
+  }
+}
