98011bd029233e209096eadfaaadf557699bb828
plosys <89532245+plosys@users.noreply.github.com>
2020-12-23 08:42:10 +0000

    continued development

--- blocks/tiles/block.ts
+++ blocks/tiles/block.ts
@@ -1,3 +1,53 @@
 import { Remapper } from '@appsemble/sdk';
 
-declare module '@appsem
+declare module '@appsemble/sdk' {
+  interface Actions {
+    /**
+     * This action is triggered when a tile is clicked.
+     */
+    onClick: never;
+  }
+
+  interface EventListeners {
+    /**
+     * On what event to listen for incoming data to display.
+     */
+    data: never;
+  }
+
+  interface Parameters {
+    /**
+     * This can be used to render an image.
+     * This can be either an asset ID or name, or a full URL.
+     *
+     * This takes precedence over an icon.
+     */
+    image?: Remapper;
+
+    /**
+     * The [Font Awesome icon](https://fontawesome.com/icons?m=free) to render.
+     *
+     * This is ignored if an asset can be resolved.
+     */
+    icon?: Remapper;
+
+    /**
+     * The label to render.
+     */
+    text?: Remapper;
+
+    /**
+     * This color is used to fill the background of the tile.
+     *
+     * Takes either a hex color or a [Bulma color](https://bulma.io/documentation/helpers/color-helpers)
+     */
+    color?: Remapper;
+  }
+
+  interface Messages {
+    /**
+     * This message is displayed if there was a problem loading the message.
+     */
+    loadError: never;
+  }
+}
---
+++ blocks/tiles/src/index.tsx
@@ -0,0 +1,74 @@
+import { bootstrap, IconName } from '@appsemble/sdk';
+
+import styles from './index.module.css';
+
+function isValidUrl(string: string): boolean {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch {
+    return false;
+  }
+
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
+const hexColor = /^#[\dA-Fa-f]{6}$/;
+const bulmaColors = new Set([
+  'danger',
+  'dark',
+  'info',
+  'link',
+  'primary',
+  'success',
+  'warning',
+  'white',
+]);
+
+bootstrap(
+  ({ actions: { onClick }, events, parameters, utils: { asset, fa, formatMessage, remap } }) => {
+    const wrapper = (
+      <div className={styles.wrapper}>
+        <div className={styles.loader} />
+      </div>
+    );
+
+    events.on.data((data, error) => {
+      while (wrapper.lastElementChild) {
+        wrapper.lastElementChild.remove();
+      }
+
+      if (error) {
+        wrapper.append(formatMessage('loadError'));
+        return;
+      }
+
+      const results = [].concat(data);
+
+      wrapper.append(
+        ...results.map((result) => {
+          const image = remap(parameters.image, result) as string;
+          const color = remap(parameters.color, result) as string;
+          const text = remap(parameters.text, result) as string;
+          const icon = remap(parameters.icon, result) as IconName;
+          const isUrl = isValidUrl(image);
+
+          const className = `${styles.tile} px-3 py-3 ${
+            bulmaColors.has(color) ? `has-background-${color}` : ''
+          }`;
+          const style = hexColor.test(color) ? { backgroundColor: color } : {};
+          const children = [
+            image ? (
+              <img alt={text} src={isUrl ? image : asset(image)} />
+            ) : icon ? (
+              <i className={`${fa(icon)} ${styles.icon}`} />
+            ) : undefined,
+            text && (
+              <p className="has-text-centered" style={style}>
+                {text}
+              </p>
+            ),
+          ];
+
+          const 
