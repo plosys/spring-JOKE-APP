f0cb935d7c5a756dd8dbc41e2c734cb67870dc1c
plosys <89532245+plosys@users.noreply.github.com>
2020-12-25 21:05:21 +0000

    Fix CHANGELOG

--- blocks/wordcloud/src/WordcloudLogic/WordcloudLogic.tsx
+++ blocks/wordcloud/src/WordcloudLogic/WordcloudLogic.tsx
@@ -24,4 +24,35 @@ interface WordcloudProps {
 export function WordcloudLogic(props: WordcloudProps): VNode {
   const canvasRef = useRef<HTMLCanvasElement>();
   const canvasWidth = Math.floor(window.innerWidth * 0.61);
-  const canvasHeight = Math.floor(window.innerHeight * 0.82)
+  const canvasHeight = Math.floor(window.innerHeight * 0.82);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+
+    const list = mapArrayToWordcloudObject(props.words);
+
+    WordCloud(canvasRef.current, {
+      color: '#000000',
+      rotateRatio: 0,
+      ...props.options,
+      list,
+    });
+  }, [props.options, props.words]);
+
+  if (!WordCloud.isSupported) {
+    return <FormattedMessage id="unsupported" />;
+  }
+
+  return (
+    <div className="canvasContainer">
+      <canvas
+        className={styles.centerCloud}
+        height={canvasHeight}
+        ref={canvasRef}
+        width={canvasWidth}
+      />
+    </div>
+  );
+}
