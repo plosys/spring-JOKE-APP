9e5c799aae0bd5644cb7613537738f3fe6da1485
plosys <89532245+plosys@users.noreply.github.com>
2021-09-21 08:39:30 +0000

    f2fs: use atomic type for node count in extent tree

---
+++ packages/cli/lib/app.ts
@@ -0,0 +1,406 @@
+
+import { createReadStream, existsSync, ReadStream } from 'node:fs';
+import { mkdir, readdir, readFile, stat } from 'node:fs/promises';
+import { join, parse, relative, resolve } from 'node:path';
+import { inspect } from 'node:util';
+
+import { AppsembleError, logger, opendirSafe, readData, writeData } from '@appsemble/node-utils';
+import { App, AppDefinition, AppsembleMessages, AppVisibility, Messages } from '@appsemble/types';
+import { extractAppMessages, has, normalizeBlockName } from '@appsemble/utils';
+import axios from 'axios';
+import FormData from 'form-data';
+
+import { AppsembleContext, AppsembleRC } from '../types.js';
+import { authenticate } from './authentication.js';
+import { traverseBlockThemes } from './block.js';
+import { coerceRemote } from './coercers.js';
+import { printAxiosError } from './output.js';
+import { processCss } from './processCss.js';
+import { createResource } from './resource.js';
+
+interface CreateAppParams {
+  /**
+   * The OAuth2 client credentials to use.
+   */
+  clientCredentials?: string;
+
+  /**
+   * If specified, the context matching this name is used, overriding command line flags.
+   */
+  context?: string;
+
+  /**
+   * The ID of the organization to upload for.
+   */
+  organization: string;
+
+  /**
+   * The path in which the App YAML is located.
+   */
+  path: string;
+
+  /**
+   * Visibility of the app in the public app store.
+   */
+  visibility: AppVisibility;
+
+  /**
+   * The remote server to create the app on.
+   */
+  remote: string;
+
+  /**
+   * Whether the App should be marked as a template.
+   */
+  template: boolean;
+
+  /**
+   * The icon to upload.
+   */
+  icon: NodeJS.ReadStream | ReadStream;
+
+  /**
+   * The background color to use for the icon in opaque contexts.
+   */
+  iconBackground: string;
+
+  /**
+   * The maskable icon to upload.
+   */
+  maskableIcon: NodeJS.ReadStream | ReadStream;
+
+  /**
+   * Whether the API should be called with a dry run.
+   */
+  dryRun: boolean;
+
+  /**
+   * Whether resources from the `resources` directory should be created after creating the app.
+   */
+  resources: boolean;
+
+  /**
+   * If the app context is specified,
+   * modify it for the current context to include the id of the created app.
+   */
+  modifyContext: boolean;
+
+  /**
+   * The ID to use for Google Analytics for the app.
+   */
+  googleAnalyticsId?: string;
+
+  /**
+   * The custom Sentry DSN for the app.
+   */
+  sentryDsn?: string;
+
+  /**
+   * The environment for the custom Sentry DSN for the app.
+   */
+  sentryEnvironment?: string;
+}
+
+interface UpdateAppParams {
+  /**
+   * The OAuth2 client credentials to use.
+   */
+  clientCredentials: string;
+
+  /**
+   * If specified, the context matching this name is used, overriding command line flags.
+   */
+  context?: string;
+
+  /**
+   * The ID of the app to update.
+   */
+  id?: number;
+
+  /**
+   * The path in which the App YAML is located.
+   */
+  path: string;
+
+  /**
+   * Visibility of the app in the public app store.
+   */
+  visibility: AppVisibility;
+
+  /**
+   * The remote server to create the app on.
+   */
+  remote: string;
+
+  /**
+   * Whether the App should be marked as a template.
+   */
+  template: boolean;
+
+  /**
+   * Whether the locked property should be ignored.
+   */
+  force: boolean;
+
+  /**
+   * The icon to upload.
+   */
+  icon: NodeJS.ReadStream | ReadStream;
+
+  /**
+   * The background color to use for the icon in opaque contexts.
+   */
+  iconBackground: string;
+
+  /**
+   * The maskable icon to upload.
+   */
+  maskableIcon: NodeJS.ReadStream | ReadStream;
+
+  /**
+   * The ID to use for Google Analytics for the app.
+   */
+  googleAnalyticsId?: string;
+
+  /**
+   * The custom Sentry DSN for the app.
+   */
+  sentryDsn?: string;
+
+  /**
+   * The environment for the custom Sentry DSN for the app.
+   */
+  sentryEnvironment?: string;
+}
+
+/**
+ * Traverses an app directory and appends the files it finds to the given FormData object.
+ *
+ * @param path The path of the app directory to traverse.
+ * @param context The Context to use from `.appsemblerc.yaml`.
+ * @param formData The FormData object to append the results into..
+ * @returns The context from `.appsemblerc.yaml` if a match was found.
+ */
+export async function traverseAppDirectory(
+  path: string,
+  context: string,
+  formData: FormData,
+): Promise<[AppsembleContext, AppsembleRC, string]> {
+  let discoveredContext: AppsembleContext;
+  let rc: AppsembleRC;
+  let iconPath: string;
+  let maskableIconPath: string;
+  let yaml: string;
+
+  logger.info(`Traversing directory for App files in ${path} 🕵`);
+  await opendirSafe(path, async (filepath, filestat) => {
+    switch (filestat.name.toLowerCase()) {
+      case '.appsemblerc.yaml':
+        logger.info(`Reading app settings from ${filepath}`);
+        [rc] = await readData<AppsembleRC>(filepath);
+        if ('iconBackground' in rc) {
+          formData.append('iconBackground', rc.iconBackground);
+        }
+        if (context && has(rc?.context, context)) {
+          discoveredContext = rc.context[context];
+          logger.verbose(`Using context: ${inspect(discoveredContext, { colors: true })}`);
+        }
+        break;
+
+      case 'app-definition.yaml': {
+        logger.info(`Using app definition from ${filepath}`);
+        if (yaml !== undefined) {
+          throw new AppsembleError('Found duplicate app definition');
+        }
+
+        const [, data] = await readData(filepath);
+        yaml = data;
+        formData.append('yaml', data);
+        return;
+      }
+
+      case 'icon.png':
+      case 'icon.svg':
+        iconPath = filepath;
+        return;
+
+      case 'maskable-icon.png':
+        maskableIconPath = filepath;
+        return;
+
+      case 'readme.md':
+        logger.info(`Including longDescription from ${filepath}`);
+        formData.append('longDescription', await readFile(filepath, 'utf8'));
+        return;
+
+      case 'screenshots':
+        return opendirSafe(
+          filepath,
+          (screenshotPath, screenshotStat) => {
+            logger.info(`Adding screenshot ${screenshotPath} 🖼️`);
+            if (!screenshotStat.isFile()) {
+              throw new AppsembleError(`Expected ${screenshotPath} to be an image file`);
+            }
+            formData.append('screenshots', createReadStream(screenshotPath));
+          },
+          { allowMissing: true },
+        );
+
+      case 'theme':
+        return opendirSafe(filepath, async (themeDir, themeStat) => {
+          const name = themeStat.name.toLowerCase();
+          if (name !== 'core' && name !== 'shared') {
+            return;
+          }
+          if (!themeStat.isDirectory()) {
+            throw new AppsembleError(`Expected ${themeDir} to be a directory`);
+          }
+          const css = await processCss(join(themeDir, 'index.css'));
+          formData.append(`${name}Style`, css);
+        });
+
+      default:
+        logger.warn(`Found unused file ${filepath}`);
+    }
+  });
+  if (yaml === undefined) {
+    throw new AppsembleError('No app definition found');
+  }
+  discoveredContext ||= {};
+  discoveredContext.icon = discoveredContext.icon
+    ? resolve(path, discoveredContext.icon)
+    : iconPath;
+  discoveredContext.maskableIcon = discoveredContext.maskableIcon
+    ? resolve(path, discoveredContext.maskableIcon)
+    : maskableIconPath;
+  return [discoveredContext, rc, yaml];
+}
+
+/**
+ * Upload messages for an app.
+ *
+ * @param path The path to the app directory.
+ * @param appId The app id to upload the messages for.
+ * @param remote The remote to upload the messages to.
+ * @param force Whether or not to force the update for locked apps.
+ */
+export async function uploadMessages(
+  path: string,
+  appId: number,
+  remote: string,
+  force: boolean,
+): Promise<void> {
+  const result: Messages[] = [];
+
+  logger.info('Searching for translations 🕵');
+  await opendirSafe(
+    join(path, 'i18n'),
+    async (messageFile) => {
+      logger.verbose(`Processing ${messageFile} ⚙️`);
+      const { name: language } = parse(messageFile);
+
+      if (result.some((entry) => entry.language === language)) {
+        throw new AppsembleError(
+          `Found duplicate language “${language}”. Make sure each language only exists once in the directory.`,
+        );
+      }
+
+      const [messages] = await readData<AppsembleMessages>(messageFile);
+      result.push({ force, language, messages });
+    },
+    { allowMissing: true },
+  );
+
+  if (!result.length) {
+    logger.info('No translations found 🤷');
+  }
+
+  for (const language of result) {
+    await axios.post(`/api/apps/${appId}/messages`, language, { baseURL: remote });
+    logger.info(`Successfully uploaded messages for language “${language.language}” 🎉`);
+  }
+}
+
+/**
+ * @param path The path to the app directory.
+ * @param languages A list of languages for which translations should be added in addition to the
+ * existing ones.
+ * @param verify A list of languages to verify.
+ * @param format The file format that should be used for the output.
+ */
+export async function writeAppMessages(
+  path: string,
+  languages: string[],
+  verify: string[],
+  format: 'json' | 'yaml',
+): Promise<void> {
+  logger.info(`Extracting messages from ${path}`);
+  let app: AppDefinition;
+  let i18nDir = join(path, 'i18n');
+  const messageFiles = new Set<string>();
+
+  await opendirSafe(path, async (filepath, filestat) => {
+    switch (filestat.name.toLowerCase()) {
+      case 'app-definition.yaml':
+        [app] = await readData<AppDefinition>(filepath);
+        break;
+      case 'i18n': {
+        // For case insensitivity
+        i18nDir = filepath;
+        const i18nFiles = await readdir(filepath);
+        for (const f of i18nFiles) {
+          messageFiles.add(join(filepath, f));
+        }
+        break;
+      }
+      default:
+        break;
+    }
+  });
+  if (!app) {
+    throw new AppsembleError(`Couldn’t find app definition for ${path}`);
+  }
+  // Ensure the i18n directory exists.
+  await mkdir(i18nDir, { recursive: true });
+
+  for (const lang of [...languages, ...verify]) {
+    messageFiles.add(join(i18nDir, `${lang}.${format}`));
+  }
+  const defaultLangFile = join(i18nDir, `${app.defaultLanguage || 'en'}.${format}`);
+  messageFiles.add(defaultLangFile);
+  const blockMessageKeys: AppsembleMessages['blocks'] = {};
+  const extractedMessages = extractAppMessages(app, (block) => {
+    const type = normalizeBlockName(block.type);
+    if (blockMessageKeys[type]) {
+      blockMessageKeys[type][block.version] = {};
+    } else {
+      blockMessageKeys[type] = {
+        [block.version]: {},
+      };
+    }
+  });
+  logger.verbose(`Found message IDs: ${inspect(extractedMessages)}`);
+  for (const filepath of messageFiles) {
+    logger.info(`Processing ${filepath}`);
+    let oldMessages: AppsembleMessages;
+    if (existsSync(filepath)) {
+      [oldMessages] = await readData<AppsembleMessages>(filepath);
+    } else if (verify) {
+      throw new AppsembleError(`Missing translations file: ${filepath}`);
+    } else {
+      oldMessages = {
+        core: {},
+        blocks: {},
+        ...extractedMessages,
+      };
+    }
+
+    const newMessageIds = Object.fromEntries(
+      Object.keys(extractedMessages.messageIds).map((key) => {
+        if (has(oldMessages.messageIds, key) && oldMessages.messageIds[key]) {
+          if (typeof oldMessages.messageIds[key] !== 'string') {
+            throw new AppsembleError(`Invalid translation key: messageIds.${key}`);
+          }
+
+          return [key, oldMessages.messageIds[key]];
+        }
