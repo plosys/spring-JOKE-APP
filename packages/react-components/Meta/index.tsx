1e7f2f6dbf76f5c27941028ee78f8855e87bf9d1
plosys <89532245+plosys@users.noreply.github.com>
2021-11-06 18:12:47 +0000

    update some comments

--- packages/react-components/Meta/index.tsx
+++ packages/react-components/Meta/index.tsx
@@ -36,4 +36,143 @@ type SetMeta = (depth: number, breadcrumb?: Breadcrumb) => void;
 type ContextType = [number, SetMeta];
 
 const MetaContext = createContext<ContextType>([0, noop]);
-const BreadcrumbContext = createContext<Breadc
+const BreadcrumbContext = createContext<Breadcrumb[]>([]);
+
+interface MetaProviderProps {
+  /**
+   * Children to be rendered within the context of this provider.
+   */
+  children: ReactNode;
+
+  /**
+   * The default page description.
+   */
+  description: Text;
+
+  /**
+   * The top level title to use.
+   */
+  title: string;
+}
+
+/**
+ * A provider for breadcrumbs and page metadata.
+ */
+export function MetaProvider({ children, description, title }: MetaProviderProps): ReactElement {
+  const [breadcrumbs, setBreadcrumbs] = useState<Breadcrumb[]>([]);
+  const descriptionNode = useRef<HTMLMetaElement>();
+  const { formatMessage } = useIntl();
+
+  const setMeta = useCallback<SetMeta>((index, newValue) => {
+    setBreadcrumbs((oldValues) => {
+      const oldValue = oldValues[index];
+      if (newValue?.title === oldValue?.title && newValue?.url === oldValue?.url) {
+        return oldValues;
+      }
+      const newValues = [...oldValues];
+      newValues[index] = newValue;
+      return newValues;
+    });
+  }, []);
+
+  useEffect(() => {
+    let result = title;
+    for (const breadcrumb of breadcrumbs) {
+      if (breadcrumb?.title) {
+        result = `${breadcrumb.title} · ${title}`;
+      }
+    }
+    document.title = result;
+  }, [breadcrumbs, title]);
+
+  useEffect(() => {
+    if (!descriptionNode.current) {
+      descriptionNode.current = document.createElement('meta');
+      descriptionNode.current.name = 'description';
+      document.head.append(descriptionNode.current);
+    }
+    const descriptions = breadcrumbs.map((breadcrumb) => breadcrumb?.description).filter(Boolean);
+    descriptionNode.current.content = descriptions.length
+      ? descriptions[descriptions.length - 1]
+      : typeof description === 'string'
+      ? description
+      : formatMessage(description);
+  }, [breadcrumbs, description, formatMessage]);
+
+  return (
+    <MetaContext.Provider value={useMemo(() => [0, setMeta], [setMeta])}>
+      <BreadcrumbContext.Provider value={breadcrumbs}>{children}</BreadcrumbContext.Provider>
+    </MetaContext.Provider>
+  );
+}
+
+/**
+ * Use the breadcrumbs that have currently been registered.
+ *
+ * @returns The currently active breadcrumbs
+ */
+export function useBreadcrumbs(): Breadcrumb[] {
+  return useContext(BreadcrumbContext).filter(Boolean);
+}
+
+/**
+ * Define a title and description for a page.
+ *
+ * @param title The page title to use. This will be used for both a breadcrumb and as part of the
+ * page title.
+ * @param description The page description to use. This is used for SEO purposes only. Therefor
+ * it’s only needed for public pages.
+ */
+export function useMeta(title: Text, description?: Text): void {
+  const [depth, setMeta] = useContext(MetaContext);
+  const { formatMessage } = useIntl();
+
+  const { pathname } = useLocation();
+  // Don't count empty string-, language- and first page segments
+  const segmentCount = pathname.split('/').length - 3;
+  const url = `.${'/..'.repeat(segmentCount > 0 ? segmentCount - depth : 0)}`;
+
+  useEffect(() => {
+    const formatMaybe = (string: Text): string =>
+      string ? (typeof string === 'string' ? string : formatMessage(string)) : undefined;
+
+    setMeta(depth, {
+      description: formatMaybe(description),
+      title: formatMaybe(title),
+      url,
+    });
+
+    return () => setMeta(depth);
+  }, [depth, description, formatMessage, setMeta, title, url]);
+}
+
+interface MetaSwitchProps {
+  /**
+   * Child routes to render.
+   */
+  children: ReactNode;
+
+  /**
+   * The page description to use.
+   */
+  description?: Text;
+
+  /**
+   * The page title to use.
+   */
+  title?: Text;
+}
+
+/**
+ * Render a React router switch and a new level for breadcrumb metadata.
+ */
+export function MetaSwitch({ children, description, title }: MetaSwitchProps): ReactElement {
+  const [depth, setMeta] = useContext(MetaContext);
+  useMeta(title, description);
+
+  return (
+    <MetaContext.Provider value={useMemo(() => [depth + 1, setMeta], [depth, setMeta])}>
+      <Routes>{children}</Routes>
+    </MetaContext.Provider>
+  );
+}
