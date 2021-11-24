1230a499c31fb38056b506aa29a48120dbb94428
plosys <89532245+plosys@users.noreply.github.com>
2021-11-24 20:05:30 +0000

    expand README

--- packages/scripts/commands/validate.ts
+++ packages/scripts/commands/validate.ts
@@ -69,4 +69,140 @@ interface Result {
 type Assert = (assertion: boolean, filename: string, message: string, workspace?: string) => void;
 
 async function validateTranslations(assert: Assert): Promise<void> {
-  const developerLocales = [default
+  const developerLocales = [defaultLocale, 'nl'].sort();
+  const translations: Record<string, Record<string, string>> = {};
+
+  await opendirSafe('./i18n', async (filepath, stat) => {
+    if (stat.name === 'index.ts') {
+      return;
+    }
+
+    if (!stat.isFile() || !filepath.endsWith('.json')) {
+      assert(false, filepath, 'should be a json file');
+      return;
+    }
+
+    const [language] = stat.name.split('.json');
+    const [messages] = await readData<Record<string, string>>(filepath);
+    translations[language] = messages;
+  });
+
+  const allLocales = [...new Set([...developerLocales, ...Object.keys(translations)])].sort();
+  const translatedMessages = await extractMessages();
+
+  for (const language of allLocales) {
+    const path = `i18n/${language}.json`;
+    const messages = translations[language];
+    if (language === defaultLocale) {
+      assert(
+        isDeepStrictEqual(messages, translatedMessages[language]),
+        path,
+        `${defaultLocale} messages should be equal when extracted`,
+      );
+    }
+
+    if (developerLocales.includes(language)) {
+      const untranslatedMessages = Object.values(messages).filter((message) => !message);
+      assert(untranslatedMessages.length === 0, path, 'Messages should be translated');
+    } else {
+      assert(
+        isDeepStrictEqual(
+          Object.keys(messages).sort(),
+          Object.keys(translatedMessages[language]).sort(),
+        ),
+        path,
+        'Keys should be the same',
+      );
+    }
+  }
+}
+
+async function validate(
+  assert: Assert,
+  { dir, pkg }: Workspace,
+  latestVersion: string,
+): Promise<void> {
+  /**
+   * Validate package.json
+   */
+  const pkgNameMatch = pkg.name.match(/^(@(?<scope>[a-z-]+)\/)?(?<name>[a-z-]+[\da-z-]+)$/);
+  assert(
+    basename(dir) === pkgNameMatch?.groups.name,
+    '',
+    'Base directory should match package name',
+  );
+  assert(
+    pkgNameMatch?.groups.scope === 'appsemble' ||
+      unscopedPackageNames.has(pkgNameMatch?.groups.name),
+    'package.json',
+    'Name should use the @appsemble scope',
+  );
+  if (basename(dirname(dir)) !== 'blocks') {
+    for (const keyword of ['app', 'apps', 'framework', 'low-code', 'lowcode']) {
+      assert(
+        pkg.keywords.includes(keyword),
+        'package.json',
+        `Keywords should at least contain ${keyword}`,
+      );
+    }
+    assert(
+      pkg.keywords.every((keyword, i) => !i || pkg.keywords[i - 1].localeCompare(keyword) < 0),
+      'package.json',
+      'Keywords should be sorted alphabetically',
+    );
+  }
+  assert(
+    pkg.type === 'module' || pkg.type === 'commonjs',
+    'package.json',
+    'Type should be explicitly set to “commonjs” or “module”',
+  );
+  assert(
+    pkg.version === latestVersion,
+    'package.json',
+    `Version should match latest version "${latestVersion}"`,
+  );
+  assert(typeof pkg.description === 'string', 'package.json', 'Description should be valid');
+  assert(
+    pkg.homepage === 'https://appsemble.app',
+    'package.json',
+    'Homepage should be "https://appsemble.app"',
+  );
+  assert(
+    pkg.bugs === 'https://gitlab.com/appsemble/appsemble/issues',
+    'package.json',
+    'Bugs should be "https://gitlab.com/appsemble/appsemble/issues"',
+  );
+  assert(
+    (pkg?.repository as any)?.type === 'git',
+    'package.json',
+    'Repository type should be "git"',
+  );
+  assert(
+    (pkg?.repository as any)?.url === 'https://gitlab.com/appsemble/appsemble.git',
+    'package.json',
+    'Repository url should be "https://gitlab.com/appsemble/appsemble.git"',
+  );
+  assert(
+    (pkg?.repository as any)?.directory === normalizePath(relative(process.cwd(), dir)),
+    'package.json',
+    `Repository directory should be "${normalizePath(relative(process.cwd(), dir))}"`,
+  );
+  assert(pkg.license === 'LGPL-3.0-only', 'package.json', 'License should be "LGPL-3.0-only"');
+  assert(
+    pkg.author === 'Appsemble <info@appsemble.com> (https://appsemble.com)',
+    'package.json',
+    'Author should be "Appsemble <info@appsemble.com> (https://appsemble.com)"',
+  );
+  assert(
+    pkg.scripts?.test === 'NODE_OPTIONS=--experimental-vm-modules jest',
+    'package.json',
+    'Test script should be "NODE_OPTIONS=--experimental-vm-modules jest"',
+  );
+  for (const version of Object.values({ ...pkg.dependencies, ...pkg.devDependencies })) {
+    if (version.startsWith('@appsemnle/')) {
+      assert(
+        version === latestVersion,
+        'package.json',
+        `Dependencies on Appsemble packages should depend on exactly "${latestVersion}"`,
+      );
+    }
