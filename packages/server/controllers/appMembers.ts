0315decbd91add0e4572bcb894463989eda4a43b
plosys <89532245+plosys@users.noreply.github.com>
2021-12-26 12:39:05 +0000

    POC

--- packages/server/controllers/appMembers.ts
+++ packages/server/controllers/appMembers.ts
@@ -160,4 +160,196 @@ export async function getAppMembers(ctx: Context): Promise<void> {
   const appMembers: AppMemberType[] = app.AppMembers.map((member) => ({
     id: member.UserId,
     name: member.name,
-    primaryEmail:
+    primaryEmail: member.email,
+    role: member.role,
+    properties: member.properties,
+  }));
+
+  if (app.definition.security?.default?.policy !== 'invite') {
+    const organization = await Organization.findByPk(app.OrganizationId, {
+      include: [
+        {
+          model: User,
+          where: { id: { [Op.not]: app.AppMembers.map((member) => member.UserId) } },
+          required: false,
+        },
+      ],
+    });
+
+    for (const user of organization.Users) {
+      appMembers.push({
+        id: user.id,
+        name: user.name,
+        primaryEmail: user.primaryEmail,
+        role: user?.AppMember?.role ?? app.definition.security.default.role,
+      });
+    }
+  }
+
+  ctx.body = appMembers;
+}
+
+export async function getAppMember(ctx: Context): Promise<void> {
+  const {
+    pathParams: { appId, memberId },
+  } = ctx;
+
+  const app = await App.findByPk(appId, {
+    attributes: ['definition'],
+    include: [
+      {
+        model: AppMember,
+        attributes: {
+          exclude: ['picture'],
+        },
+        where: { UserId: memberId },
+        required: false,
+      },
+    ],
+  });
+  if (!app) {
+    throw notFound('App not found');
+  }
+
+  if (app.definition.security === undefined) {
+    throw notFound('App does not have a security definition');
+  }
+
+  if (app.AppMembers.length !== 1) {
+    throw notFound('App member not found');
+  }
+
+  const [member] = app.AppMembers;
+
+  ctx.body = {
+    id: member.UserId,
+    name: member.name,
+    primaryEmail: member.email,
+    role: member.role,
+  };
+}
+
+export async function setAppMember(ctx: Context): Promise<void> {
+  const {
+    pathParams: { appId, memberId },
+    request: {
+      body: { properties, role },
+    },
+  } = ctx;
+
+  const app = await App.findByPk(appId, {
+    attributes: ['OrganizationId', 'definition', 'id'],
+    include: [
+      {
+        model: AppMember,
+        attributes: {
+          exclude: ['picture'],
+        },
+        required: false,
+        where: { UserId: memberId },
+      },
+    ],
+  });
+  if (!app) {
+    throw notFound('App not found');
+  }
+
+  await checkRole(ctx, app.OrganizationId, Permission.EditApps);
+
+  const user = await User.findByPk(memberId);
+  if (!user) {
+    throw notFound('User with this ID doesn’t exist.');
+  }
+
+  if (!has(app.definition.security.roles, role)) {
+    throw badRequest(`Role ‘${role}’ is not defined.`);
+  }
+
+  let member = app.AppMembers?.[0];
+
+  if (member) {
+    member.role = role;
+    if (properties) {
+      member.properties = properties;
+    }
+    await member.save();
+  } else {
+    member = await AppMember.create({
+      UserId: user.id,
+      AppId: app.id,
+      role,
+      properties,
+    });
+  }
+
+  ctx.body = {
+    id: user.id,
+    name: member.name,
+    primaryEmail: member.email,
+    role,
+    properties,
+  };
+}
+
+export async function getAppAccounts(ctx: Context): Promise<void> {
+  const { user } = ctx;
+  const { baseLanguage, language, query } = parseLanguage(ctx.query?.language);
+
+  const apps = await App.findAll(createAppAccountQuery(user, query));
+
+  ctx.body = apps.map((app) => outputAppMember(app, language, baseLanguage));
+}
+
+export async function getAppAccount(ctx: Context): Promise<void> {
+  const {
+    pathParams: { appId },
+    user,
+  } = ctx;
+  const { baseLanguage, language, query } = parseLanguage(ctx.query?.language);
+
+  const app = await App.findOne({
+    where: { id: appId },
+    ...createAppAccountQuery(user, query),
+  });
+
+  if (!app) {
+    throw notFound('App account not found');
+  }
+
+  ctx.body = outputAppMember(app, language, baseLanguage);
+}
+
+export async function patchAppAccount(ctx: Context): Promise<void> {
+  const {
+    mailer,
+    pathParams: { appId },
+    request: {
+      body: { email, locale, name, picture, properties },
+    },
+    user,
+  } = ctx;
+  const { baseLanguage, language, query } = parseLanguage(ctx.query?.language);
+
+  const app = await App.findOne({
+    where: { id: appId },
+    ...createAppAccountQuery(user, query),
+  });
+
+  if (!app) {
+    throw notFound('App account not found');
+  }
+
+  const [member] = app.AppMembers;
+  const result: Partial<AppMember> = {};
+  if (email != null && member.email !== email) {
+    result.email = email;
+    result.emailVerified = false;
+    result.emailKey = randomBytes(40).toString('hex');
+
+    const verificationUrl = new URL('/Verify', getAppUrl(app));
+    verificationUrl.searchParams.set('token', result.emailKey);
+
+    mailer
+      .sendTranslatedEmail({
+        appId,
+ 
