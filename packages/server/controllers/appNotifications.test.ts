ef8d72cb7170f36b013bb0cf96889d8ca79a5b1a
plosys <89532245+plosys@users.noreply.github.com>
2022-01-10 15:49:44 +0000

    Add information to README

--- packages/server/controllers/appNotifications.test.ts
+++ packages/server/controllers/appNotifications.test.ts
@@ -64,4 +64,176 @@ useTestDatabase(import.meta);
 
 beforeAll(async () => {
   setArgv({ host: 'http://localhost', secret: 'test' });
-  
+  const server = await createServer();
+  await setTestApp(server);
+});
+
+beforeEach(async () => {
+  import.meta.jest.useFakeTimers({ now: 0 });
+
+  user = await createTestUser();
+  organization = await Organization.create({
+    id: 'testorganization',
+    name: 'Test Organization',
+  });
+  await Member.create({ OrganizationId: organization.id, UserId: user.id, role: 'Owner' });
+});
+
+describe('getSubscription', () => {
+  it('should subscription statuses to resources', async () => {
+    const app = await defaultApp(organization.id);
+
+    await AppSubscription.create({
+      AppId: app.id,
+      endpoint: 'https://example.com',
+      p256dh: 'abc',
+      auth: 'def',
+    });
+
+    const resource = await Resource.create({
+      AppId: app.id,
+      type: 'person',
+      data: { foo: 'I am Foo.' },
+    });
+
+    authorizeApp(app);
+    await request.patch(`/api/apps/${app.id}/subscriptions`, {
+      endpoint: 'https://example.com',
+      resource: 'person',
+      resourceId: resource.id,
+      action: 'update',
+      value: true,
+    });
+
+    const response = await request.get(`/api/apps/${app.id}/subscriptions`, {
+      params: { endpoint: 'https://example.com' },
+    });
+
+    expect(response).toMatchInlineSnapshot(`
+      HTTP/1.1 200 OK
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "person": {
+          "create": false,
+          "delete": false,
+          "subscriptions": {
+            "1": {
+              "delete": false,
+              "update": true,
+            },
+          },
+          "update": false,
+        },
+        "pet": {
+          "create": false,
+          "delete": false,
+          "update": false,
+        },
+      }
+    `);
+  });
+
+  it('should 404 on non-existent subscriptions', async () => {
+    const app = await defaultApp(organization.id);
+    const response = await request.get(`/api/apps/${app.id}/subscriptions`, {
+      params: { endpoint: 'https://example.com' },
+    });
+
+    expect(response).toMatchInlineSnapshot(`
+      HTTP/1.1 404 Not Found
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "error": "Not Found",
+        "message": "Subscription not found",
+        "statusCode": 404,
+      }
+    `);
+  });
+});
+
+describe('addSubscription', () => {
+  it('should subscribe to apps', async () => {
+    const app = await defaultApp(organization.id);
+
+    authorizeApp(app);
+    const response = await request.post(`/api/apps/${app.id}/subscriptions`, {
+      endpoint: 'https://example.com',
+      keys: { p256dh: 'abc', auth: 'def' },
+    });
+
+    const subscription = await AppSubscription.findOne({
+      where: { endpoint: 'https://example.com' },
+      raw: true,
+    });
+
+    expect(response).toMatchInlineSnapshot('HTTP/1.1 204 No Content');
+    expect(subscription).toMatchObject({
+      endpoint: 'https://example.com',
+      p256dh: 'abc',
+      auth: 'def',
+      AppId: app.id,
+      UserId: user.id,
+      created: new Date(),
+      updated: new Date(),
+    });
+  });
+});
+
+describe('updateSubscription', () => {
+  it('should update resource type subscription settings', async () => {
+    const app = await defaultApp(organization.id);
+    await AppSubscription.create({
+      AppId: app.id,
+      endpoint: 'https://example.com',
+      p256dh: 'abc',
+      auth: 'def',
+    });
+
+    authorizeApp(app);
+    const response = await request.patch(`/api/apps/${app.id}/subscriptions`, {
+      endpoint: 'https://example.com',
+      resource: 'person',
+      action: 'create',
+      value: true,
+    });
+
+    const responseB = await request.get(`/api/apps/${app.id}/subscriptions`, {
+      params: { endpoint: 'https://example.com' },
+    });
+
+    const subscription = await AppSubscription.findOne({
+      where: { endpoint: 'https://example.com' },
+    });
+
+    expect(response).toMatchInlineSnapshot('HTTP/1.1 204 No Content');
+    expect(responseB).toMatchInlineSnapshot(`
+      HTTP/1.1 200 OK
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "person": {
+          "create": true,
+          "delete": false,
+          "update": false,
+        },
+        "pet": {
+          "create": false,
+          "delete": false,
+          "update": false,
+        },
+      }
+    `);
+    expect(subscription.UserId).toStrictEqual(user.id);
+  });
+
+  it('should update individual resource subscription settings', async () => {
+    const app = await defaultApp(organization.id);
+    await AppSubscription.create({
+      AppId: app.id,
+      endpoint: 'https://example.com',
+      p256dh: 'abc',
+      auth: 'def',
+    });
+    const { id } =
