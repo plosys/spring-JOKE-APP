36da23b08650e86a64e1206c20ec4ed802229f79
plosys <89532245+plosys@users.noreply.github.com>
2022-01-16 22:48:40 +0000

    Added

--- packages/server/controllers/assets.test.ts
+++ packages/server/controllers/assets.test.ts
@@ -419,4 +419,162 @@ describe('getAssetById', () => {
     expect(response).toMatchObject({
       status: 200,
       headers: expect.objectContaining({
-        'content-type': 'text/p
+        'content-type': 'text/plain',
+        'content-disposition': `attachment; filename="${asset.id}.txt"`,
+      }),
+      data,
+    });
+  });
+
+  it('should not fetch assets from other apps', async () => {
+    const appB = await App.create({
+      definition: {
+        name: 'Test App',
+        defaultPage: 'Test Page',
+        security: {
+          default: {
+            role: 'Reader',
+            policy: 'everyone',
+          },
+          roles: {
+            Reader: {},
+          },
+        },
+      },
+      path: 'test-app-B',
+      vapidPublicKey: 'a',
+      vapidPrivateKey: 'b',
+      OrganizationId: organization.id,
+    });
+    const data = Buffer.from('buffer');
+    const asset = await Asset.create({
+      AppId: appB.id,
+      mime: 'application/octet-stream',
+      filename: 'test.bin',
+      data,
+    });
+
+    const response = await request.get(`/api/apps/${app.id}/assets/${asset.id}`);
+
+    expect(response).toMatchInlineSnapshot(`
+      HTTP/1.1 404 Not Found
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "error": "Not Found",
+        "message": "Asset not found",
+        "statusCode": 404,
+      }
+    `);
+  });
+
+  it('should fetch assets from apps that don’t exist', async () => {
+    const response = await request.get('/api/apps/0/assets/0');
+
+    expect(response).toMatchInlineSnapshot(`
+      HTTP/1.1 404 Not Found
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "error": "Not Found",
+        "message": "App not found",
+        "statusCode": 404,
+      }
+    `);
+  });
+});
+
+describe('createAsset', () => {
+  it('should be able to create an asset', async () => {
+    const data = Buffer.from([0xc0, 0xff, 0xee, 0xba, 0xbe]);
+    const response = await request.post<Asset>(
+      `/api/apps/${app.id}/assets`,
+      createFormData({ file: data, name: 'test-asset' }),
+    );
+    expect(response).toMatchInlineSnapshot(
+      { data: { id: expect.stringMatching(uuid4Pattern) } },
+      `
+      HTTP/1.1 201 Created
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "id": StringMatching /\\^\\[\\\\d\\[a-f\\]\\{8\\}-\\[\\\\da-f\\]\\{4\\}-4\\[\\\\da-f\\]\\{3\\}-\\[\\\\da-f\\]\\{4\\}-\\[\\\\d\\[a-f\\]\\{12\\}\\$/,
+        "mime": "application/octet-stream",
+        "name": "test-asset",
+      }
+    `,
+    );
+
+    const asset = await Asset.findByPk(response.data.id);
+    expect(asset).toMatchObject({
+      AppId: app.id,
+      data,
+      filename: null,
+      mime: 'application/octet-stream',
+      name: 'test-asset',
+      UserId: null,
+    });
+  });
+
+  it('should not allow using conflicting names', async () => {
+    await request.post(
+      `/api/apps/${app.id}/assets`,
+      createFormData({ file: Buffer.alloc(0), name: 'conflict' }),
+    );
+    const response = await request.post(
+      `/api/apps/${app.id}/assets`,
+      createFormData({ file: Buffer.alloc(0), name: 'conflict' }),
+    );
+    expect(response).toMatchInlineSnapshot(`
+      HTTP/1.1 409 Conflict
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "error": "Conflict",
+        "message": "An asset named conflict already exists",
+        "statusCode": 409,
+      }
+    `);
+  });
+
+  it('should accept empty files', async () => {
+    const response = await request.post(
+      `/api/apps/${app.id}/assets`,
+      createFormData({ file: Buffer.alloc(0) }),
+    );
+    expect(response).toMatchInlineSnapshot(
+      { data: { id: expect.stringMatching(uuid4Pattern) } },
+      `
+      HTTP/1.1 201 Created
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "id": StringMatching /\\^\\[\\\\d\\[a-f\\]\\{8\\}-\\[\\\\da-f\\]\\{4\\}-4\\[\\\\da-f\\]\\{3\\}-\\[\\\\da-f\\]\\{4\\}-\\[\\\\d\\[a-f\\]\\{12\\}\\$/,
+        "mime": "application/octet-stream",
+      }
+    `,
+    );
+  });
+
+  it('should support filenames', async () => {
+    const response = await request.post(
+      `/api/apps/${app.id}/assets`,
+      createFormData({ file: createFixtureStream('10x50.png') }),
+    );
+    expect(response).toMatchInlineSnapshot(
+      { data: { id: expect.stringMatching(uuid4Pattern) } },
+      `
+      HTTP/1.1 201 Created
+      Content-Type: application/json; charset=utf-8
+
+      {
+        "filename": "10x50.png",
+        "id": StringMatching /\\^\\[\\\\d\\[a-f\\]\\{8\\}-\\[\\\\da-f\\]\\{4\\}-4\\[\\\\da-f\\]\\{3\\}-\\[\\\\da-f\\]\\{4\\}-\\[\\\\d\\[a-f\\]\\{12\\}\\$/,
+        "mime": "image/png",
+      }
+    `,
+    );
+  });
+
+  it('should not create assets for apps that don’t exist', async () => {
+   
