97580c4d84d85425fcc5e924cd7887acb85bf4b4
plosys <89532245+plosys@users.noreply.github.com>
2022-01-19 14:16:12 +0000

    improved README.md

--- packages/server/controllers/blocks.ts
+++ packages/server/controllers/blocks.ts
@@ -92,4 +92,149 @@ export async function queryBlocks(ctx: Context): Promise<void> {
       o.icon IS NOT NULL as "hasOrganizationIcon",
       o.updated AS "organizationUpdated"
     FROM "BlockVersion" bv
-    INNER JOIN "Organization" o ON o.id 
+    INNER JOIN "Organization" o ON o.id = bv."OrganizationId"
+    WHERE bv.created IN (
+      SELECT MAX(created)
+      FROM "BlockVersion"
+      GROUP BY "OrganizationId", name
+    )`,
+    { type: QueryTypes.SELECT },
+  );
+
+  ctx.body = await createBlockVersionResponse(ctx, blockVersions, (blockVersion) => {
+    const {
+      OrganizationId,
+      actions,
+      description,
+      events,
+      examples,
+      hasIcon,
+      hasOrganizationIcon,
+      layout,
+      longDescription,
+      name,
+      organizationUpdated,
+      parameters,
+      version,
+      wildcardActions,
+    } = blockVersion;
+    let iconUrl = null;
+    if (hasIcon) {
+      iconUrl = `/api/blocks/@${OrganizationId}/${name}/versions/${version}/icon`;
+    } else if (hasOrganizationIcon) {
+      iconUrl = `/api/organizations/${OrganizationId}/icon?updated=${organizationUpdated.toISOString()}`;
+    }
+    return {
+      name: `@${OrganizationId}/${name}`,
+      description,
+      longDescription,
+      version,
+      actions,
+      events,
+      examples,
+      iconUrl,
+      layout,
+      parameters,
+      wildcardActions,
+    };
+  });
+}
+
+interface PublishBlockBody extends Omit<BlockManifest, 'files'> {
+  files: File[];
+  icon: File;
+  examples: string[];
+}
+
+export async function publishBlock(ctx: Context): Promise<void> {
+  const { files, icon, messages, ...data }: PublishBlockBody = ctx.request.body;
+  const { name, version } = data;
+  const actionKeyRegex = /^[a-z]\w*$/;
+
+  const [org, blockId] = name.split('/');
+  const OrganizationId = org.slice(1);
+
+  if (data.actions) {
+    for (const key of Object.keys(data.actions)) {
+      if (!actionKeyRegex.test(key) && key !== '$any') {
+        throw badRequest(`Action “${key}” does match /${actionKeyRegex.source}/`);
+      }
+    }
+  }
+
+  if (data.examples?.length) {
+    const validator = new Validator();
+    validator.customFormats.fontawesome = () => true;
+    validator.customFormats.remapper = () => true;
+    validator.customFormats.action = () => true;
+    validator.customFormats['event-listener'] = () => true;
+    validator.customFormats['event-emitter'] = () => true;
+
+    for (const exampleString of data.examples) {
+      let example: BlockDefinition;
+      try {
+        example = parse(exampleString);
+      } catch {
+        throw badRequest(`Error parsing YAML example:\n${exampleString}`);
+      }
+      if (!example || typeof example !== 'object') {
+        continue;
+      }
+      const { required, ...blockSchema } = structuredClone(
+        ctx.openApi.document.components.schemas.BlockDefinition,
+      ) as OpenAPIV3.NonArraySchemaObject;
+
+      delete blockSchema.properties.name;
+      delete blockSchema.properties.version;
+
+      const actionsSchema = blockSchema.properties.actions as OpenAPIV3.NonArraySchemaObject;
+
+      delete actionsSchema.additionalProperties;
+      if (example.actions) {
+        actionsSchema.properties = Object.fromEntries(
+          Object.keys(example.actions).map((key) => [
+            key,
+            { $ref: '#/components/schemas/ActionDefinition' },
+          ]),
+        );
+      }
+      const blockEventsSchema: OpenAPIV3.NonArraySchemaObject = {
+        type: 'object',
+        additionalProperties: false,
+        properties: {},
+      };
+      blockSchema.properties.events = blockEventsSchema;
+      if (example.events) {
+        if (example.events.emit) {
+          blockEventsSchema.properties.emit = has(example.events.emit, '$any')
+            ? { type: 'object', additionalProperties: { type: 'string' } }
+            : {
+                type: 'object',
+                properties: Object.fromEntries(
+                  Object.keys(example.events.emit).map((emitter) => [emitter, { type: 'string' }]),
+                ),
+              };
+        }
+        if (example.events.listen) {
+          blockEventsSchema.properties.listen = has(example.events.listen, '$any')
+            ? { type: 'object', additionalProperties: { type: 'string' } }
+            : {
+                type: 'object',
+                properties: Object.fromEntries(
+                  Object.keys(example.events.listen).map((listener) => [
+                    listener,
+                    { type: 'string' },
+                  ]),
+                ),
+              };
+        }
+      }
+
+      const validationResult = ctx.openApi.validate(example, blockSchema, { throw: false });
+      handleValidatorResult(validationResult, 'Validation failed for block example');
+    }
+  }
+
+  if (messages) {
+    const messageKeys = Object.keys(messages.en);
+    for (const [language, record] of Object.
