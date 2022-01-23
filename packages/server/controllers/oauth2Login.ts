d641d37689cecb1ebc3723b1cfebfa7917c09308
plosys <89532245+plosys@users.noreply.github.com>
2022-01-23 11:15:40 +0000

    Version bump to 0.9.7

--- packages/server/controllers/oauth2Login.ts
+++ packages/server/controllers/oauth2Login.ts
@@ -57,4 +57,140 @@ export async function registerOAuth2Connection(ctx: Context): Promise<void> {
 
   if (preset === googlePreset) {
     clientId = argv.googleClientId;
-    clientS
+    clientSecret = argv.googleClientSecret;
+  } else if (preset === gitlabPreset) {
+    clientId = argv.gitlabClientId;
+    clientSecret = argv.gitlabClientSecret;
+  } else if (preset === githubPreset) {
+    clientId = argv.githubClientId;
+    clientSecret = argv.githubClientSecret;
+  }
+
+  if (!clientId || !clientSecret) {
+    throw notImplemented('Unknown authorization URL');
+  }
+
+  // Exchange the authorization code for an access token and refresh token.
+  const {
+    access_token: accessToken,
+    id_token: idToken,
+    refresh_token: refreshToken,
+  } = await getAccessToken(
+    preset.tokenUrl,
+    code,
+    String(new URL('/callback', argv.host)),
+    clientId,
+    clientSecret,
+  );
+
+  const { sub, ...userInfo } = await getUserInfo(
+    accessToken,
+    idToken,
+    preset.userInfoUrl,
+    preset.remapper,
+    preset.userEmailsUrl,
+  );
+
+  const authorization = await OAuthAuthorization.findOne({
+    where: { authorizationUrl, sub },
+  });
+  if (authorization?.UserId) {
+    const dbUser = await User.findOne({ where: { id: authorization.UserId } });
+    if (dbUser && !dbUser.primaryEmail && userInfo.email) {
+      await transactional(async (transaction) => {
+        await dbUser.update({ primaryEmail: userInfo.email }, { transaction });
+        await processEmailAuthorization(
+          mailer,
+          dbUser.id,
+          userInfo.name,
+          userInfo.email,
+          Boolean(userInfo.email_verified),
+          transaction,
+        );
+      });
+    }
+    // If the combination of authorization url and sub exists, update the entry and allow the user
+    // to login to Appsemble.
+    await authorization.update({ accessToken, refreshToken }, { where: { authorizationUrl, sub } });
+    ctx.body = createJWTResponse(authorization.UserId);
+  } else {
+    // Otherwise, register an authorization object and ask the user if this is the account they want
+    // to use.
+    await (authorization
+      ? authorization.update({ accessToken, code, refreshToken })
+      : OAuthAuthorization.create({
+          accessToken,
+          authorizationUrl: preset.authorizationUrl,
+          code,
+          refreshToken,
+          sub,
+        }));
+    ctx.body = userInfo;
+  }
+}
+
+export async function connectPendingOAuth2Profile(ctx: Context): Promise<void> {
+  let {
+    mailer,
+    request: {
+      body: { authorizationUrl, code, timezone },
+    },
+    user,
+  } = ctx;
+  const preset = presets.find((p) => p.authorizationUrl === authorizationUrl);
+
+  if (!preset) {
+    throw notImplemented('Unknown authorization URL');
+  }
+
+  const authorization = await OAuthAuthorization.findOne({ where: { code, authorizationUrl } });
+
+  if (!authorization) {
+    throw notFound('No pending OAuth2 authorization found for given state');
+  }
+
+  // The user is already logged in to Appsemble.
+  if (user) {
+    // The OAuth2 authorization is not yet linked to a user, so we link it to the authenticated
+    // user. From now on the user will be able to login to this account using this OAuth2 provider.
+    if (!authorization.UserId) {
+      await authorization.update({ UserId: user.id, code: null });
+    }
+
+    // The authorization is already linked to another account, so we disallow linking it again.
+    else if (authorization.UserId !== user.id) {
+      throw forbidden('This OAuth2 authorization is already linked to an account.');
+    }
+  }
+  // The user is not yet logged in, so they are trying to register a new account using this OAuth2
+  // provider.
+  else {
+    const userInfo = await getUserInfo(
+      authorization.accessToken,
+      null,
+      preset.userInfoUrl,
+      preset.remapper,
+      preset.userEmailsUrl,
+    );
+    await transactional(async (transaction) => {
+      user = await User.create(
+        {
+          name: userInfo.name,
+          primaryEmail: userInfo.email,
+          timezone: userInfo.zoneinfo || timezone,
+        },
+        { transaction },
+      );
+      await authorization.update({ UserId: user.id }, { transaction });
+      if (userInfo.email) {
+        // We’ll try to link this email address to the new user, even though no password has been
+        // set.
+        const emailAuthorization = await EmailAuthorization.findOne({
+          where: { email: userInfo.email.toLowerCase() },
+        });
+        if (emailAuthorization) {
+          if (emailAuthorization.UserId !== user.id) {
+            throw conflict('This email address has already been linked to an existing account.');
+          }
+        } else {
+          await p
