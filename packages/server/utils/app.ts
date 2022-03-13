ac4734294d235c48e6b2167599afc6aaf47ad127
plosys <89532245+plosys@users.noreply.github.com>
2022-03-13 09:18:47 +0000

    ummm

--- packages/server/utils/app.ts
+++ packages/server/utils/app.ts
@@ -71,4 +71,158 @@ export async function getApp(
       .split('.');
     if (subdomain.length === 1) {
       [value.organizationId] = subdomain;
-    } else if (subdo
+    } else if (subdomain.length === 2) {
+      [value.appPath, value.organizationId] = subdomain;
+      value.app = await App.findOne({
+        ...queryOptions,
+        where: { path: value.appPath, OrganizationId: value.organizationId },
+      });
+    }
+  } else {
+    value.app = await App.findOne({
+      ...queryOptions,
+      where: { domain: hostname },
+    });
+  }
+  return value;
+}
+
+export function getAppUrl(app: App): URL {
+  const url = new URL(argv.host);
+  url.hostname = app.domain || `${app.path}.${app.OrganizationId}.${url.hostname}`;
+  return url;
+}
+
+/**
+ * Get a context for remappers based on an app definition.
+ *
+ * This allows to use remappers with the context of an app on the server.
+ *
+ * @param app The app for which to get the remapper context.
+ * @param language The preferred language for the context.
+ * @param userInfo The OAuth2 compatible user information.
+ * @returns A localized remapper context for the app.
+ */
+export async function getRemapperContext(
+  app: App,
+  language: string,
+  userInfo: UserInfo,
+): Promise<RemapperContext> {
+  const languages = language.toLowerCase().split(/-/g);
+  const appMessages = await AppMessages.findAll({
+    order: [['language', 'desc']],
+    where: {
+      AppId: app.id,
+      language: { [Op.or]: languages.map((lang, i) => languages.slice(0, i + 1).join('-')) },
+    },
+  });
+
+  const cache = objectCache(
+    (message) =>
+      new IntlMessageFormat(message, language, undefined, {
+        formatters: {
+          getNumberFormat,
+          getPluralRules,
+          // @ts-expect-error @formatjs/fast-memoize is typed for faux ESM
+          getDateTimeFormat: memoize.default(
+            (locale: string, opts: Intl.DateTimeFormatOptions) =>
+              new Intl.DateTimeFormat(locale, { ...opts, timeZone: userInfo?.zoneinfo }),
+          ),
+        },
+      }),
+  );
+  const appUrl = String(getAppUrl(app));
+
+  return {
+    appId: app.id,
+    appUrl,
+    url: appUrl,
+    getMessage({ defaultMessage, id }) {
+      const msg = appMessages.find(({ messages }) => has(messages.messageIds, id));
+      const message = msg ? msg.messages.messageIds[id] : defaultMessage;
+      return cache(message || `'{${id}}'`);
+    },
+    userInfo,
+    context: {},
+    locale: userInfo?.locale ?? app.definition.defaultLanguage ?? defaultLocale,
+  };
+}
+
+/**
+ * Sort apps by rating, in descending order.
+ *
+ * @param a The first app to compare.
+ * @param b The second app to compare.
+ * @returns Whether the first or second app goes first in terms of sorting.
+ */
+export function compareApps(a: App, b: App): number {
+  if (a.RatingAverage != null && b.RatingAverage != null) {
+    return b.RatingAverage - a.RatingAverage || b.RatingCount - a.RatingCount;
+  }
+
+  if (a.RatingAverage == null && b.RatingAverage == null) {
+    return 0;
+  }
+
+  return a.RatingAverage == null ? 1 : -1;
+}
+
+/**
+ * Extracts and parses the language from the query string of a request.
+ *
+ * @param input The language string to parse.
+ * @returns An object containing the language, base language, and Sequelize filter
+ * to filter AppMessages by these languages.
+ */
+export function parseLanguage(input: string[] | string): {
+  language: string;
+  baseLanguage: string;
+  query: IncludeOptions[];
+} {
+  const language = Array.isArray(input) ? input[0] : input;
+  if (!language) {
+    return { language: undefined, baseLanguage: undefined, query: [] };
+  }
+
+  if (!tags.check(language)) {
+    throw badRequest(`Language “${language}” is invalid`);
+  }
+
+  const lang = language?.toLowerCase();
+  const baseLanguage = String(
+    tags(language)
+      .subtags()
+      .find((sub) => sub.type() === 'language'),
+  ).toLowerCase();
+  const query = (
+    language
+      ? [
+          {
+            model: AppMessages,
+            where: {
+              language:
+                baseLanguage && baseLanguage !== lang ? { [Op.or]: [baseLanguage, lang] } : lang,
+            },
+            required: false,
+          },
+        ]
+      : []
+  ) as IncludeOptions[];
+
+  return { language: lang, baseLanguage, query };
+}
+
+/**
+ * Applies `messages` property to an app model instance
+ * based on the included `AppMessages` and languages.
+ *
+ * Note that this mutates `app`.
+ *
+ * @param app The app to apply the messages to.
+ * @param language The language to search for within AppMessages.
+ * @param baseLanguage The base language to search for within AppMessages.
+ */
+export function applyAppMessages(app: App, language: string, baseLanguage: string): void {
+  if (app.AppMessages?.length) {
+    const baseMessages =
+      b
