7e322a5edaea7e7837963e8792fd6c99ede90367
plosys <89532245+plosys@users.noreply.github.com>
2022-04-12 13:08:25 +0000

    update .podspec

--- packages/server/utils/model.ts
+++ packages/server/utils/model.ts
@@ -23,3 +23,40 @@ export function resolveIconUrl(app: models.App): string {
       updated: app.updated.toISOString(),
     })}`;
   }
+
+  const organizationHasIcon = app.Organization?.get('hasIcon');
+  if (organizationHasIcon) {
+    return `/api/organizations/${app.OrganizationId}/icon?${new URLSearchParams({
+      background: app.iconBackground || '#ffffff',
+      maskable: 'true',
+      updated: app.Organization.updated.toISOString(),
+    })}`;
+  }
+
+  return null;
+}
+
+export async function createOAuth2AuthorizationCode(
+  app: models.App,
+  redirectUri: string,
+  scope: string,
+  user: models.User,
+): Promise<types.OAuth2AuthorizationCode> {
+  const appHost = `${app.path}.${app.OrganizationId}.${new URL(argv.host).hostname}`;
+  const redirectHost = new URL(redirectUri).hostname;
+  if (redirectHost !== appHost && redirectHost !== app.domain) {
+    throw forbidden('Invalid redirectUri');
+  }
+
+  const { code } = await models.OAuth2AuthorizationCode.create({
+    AppId: app.id,
+    code: randomBytes(12).toString('hex'),
+    expires: addMinutes(new Date(), 10),
+    redirectUri,
+    scope,
+    UserId: user.id,
+  });
+  return {
+    code,
+  };
+}
---
+++ packages/server/utils/oauth2.test.ts
@@ -0,0 +1,151 @@
+import { AppsembleError } from '@appsemble/node-utils';
+import axios, { AxiosRequestConfig } from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import jwt from 'jsonwebtoken';
+
+import { getAccessToken, getUserInfo, hasScope } from './oauth2.js';
+
+const mock = new MockAdapter(axios);
+
+afterEach(() => {
+  mock.reset();
+});
+
+describe('hasScope', () => {
+  it('should return true if the scopes are an exact match', () => {
+    const result = hasScope('foo', 'foo');
+    expect(result).toBe(true);
+  });
+
+  it('should return true if all required scopes exist in the granted scopes', () => {
+    const result = hasScope('foo bar', 'foo');
+    expect(result).toBe(true);
+  });
+
+  it('should return false if some of the required scopes aren’t granted', () => {
+    const result = hasScope('foo', 'foo bar');
+    expect(result).toBe(false);
+  });
+});
+
+describe('getAccessToken', () => {
+  it('should make an access token request', async () => {
+    let config: AxiosRequestConfig;
+    mock.onPost('https://example.com/token').reply((req) => {
+      config = req;
+      return [200, { access_token: 'token' }];
+    });
+    const result = await getAccessToken(
+      'https://example.com/token',
+      'test_code',
+      'http://localhost/callback',
+      'test_id',
+      'test_secret',
+    );
+    expect(config.data).toBe(
+      'grant_type=authorization_code&client_id=test_id&client_secret=test_secret&code=test_code&redirect_uri=http%3A%2F%2Flocalhost%2Fcallback',
+    );
+    expect(config.headers).toMatchObject({
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+      authorization: 'Basic dGVzdF9pZDp0ZXN0X3NlY3JldA==',
+    });
+    expect(result).toStrictEqual({ access_token: 'token' });
+  });
+});
+
+describe('getUserInfo', () => {
+  it('should read information from the id token', async () => {
+    const userInfo = await getUserInfo(
+      '',
+      jwt.sign(
+        {
+          email: 'me@example.com',
+          email_verified: true,
+          name: 'Me',
+          profile: 'https://example.com/me',
+          picture: 'https://example.com/me.png',
+          sub: '42',
+          zoneinfo: 'Europe/Amsterdam',
+        },
+        'secret',
+      ),
+    );
+    expect(userInfo).toStrictEqual({
+      email: 'me@example.com',
+      email_verified: true,
+      name: 'Me',
+      profile: 'https://example.com/me',
+      picture: 'https://example.com/me.png',
+      sub: '42',
+      locale: undefined,
+      zoneinfo: 'Europe/Amsterdam',
+    });
+  });
+
+  it('should fall back to the access token', async () => {
+    const userInfo = await getUserInfo(
+      jwt.sign({ sub: '1337' }, 'secret'),
+      jwt.sign(
+        {
+          email: 'user@example.com',
+          name: 'User',
+          profile: 'https://example.com/user',
+          picture: 'https://example.com/user.png',
+          locale: undefined,
+          zoneinfo: undefined,
+        },
+        'secret',
+      ),
+    );
+    expect(userInfo).toStrictEqual({
+      email: 'user@example.com',
+      email_verified: false,
+      name: 'User',
+      profile: 'https://example.com/user',
+      picture: 'https://example.com/user.png',
+      sub: '1337',
+      locale: undefined,
+      zoneinfo: undefined,
+    });
+  });
+
+  it('should fall back to user info endpoint', async () => {
+    mock.onGet('/userinfo').reply(() => [
+      200,
+      {
+        email: 'user@example.com',
+        email_verified: false,
+        name: 'User',
+        profile: 'https://example.com/user',
+        picture: 'https://example.com/user.png',
+        locale: undefined,
+        zoneinfo: undefined,
+      },
+    ]);
+    const userInfo = await getUserInfo('', jwt.sign({ sub: '1337' }, 'secret'), '/userinfo');
+    expect(userInfo).toStrictEqual({
+      email: 'user@example.com',
+      email_verified: false,
+      name: 'User',
+      profile: 'https://example.com/user',
+      picture: 'https://example.com/user.png',
+      sub: '1337',
+      locale: undefined,
+      zoneinfo: undefined,
+    });
+  });
+
+  it('should support a remapper for the user info endpoint', async () => {
+    mock.onGet('/user').reply(() => [
+      200,
+      {
+        emailAddress: 'user@example.com',
+        fullName: 'User',
+        profileUrl: 'https://example.com/user',
+        avatarUrl: 'https://example.com/user.png',
+        userId: 1337,
+        zoneinfo: undefined,
+      },
+    ]);
+    const userInfo = await getUserInfo('', null, '
