6e9b56358d2a0d948da224c085324092573b8796
plosys <89532245+plosys@users.noreply.github.com>
2022-04-13 20:01:57 +0000

    toolbar

--- packages/server/utils/odata.ts
+++ packages/server/utils/odata.ts
@@ -83,4 +83,135 @@ const functions: Record<string, MethodConverter> = {
   matchesPattern: [[Edm.String, Edm.String], whereFunction(Op.iRegexp)],
 
   // https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_tolower
-  tolower: [[Edm.Str
+  tolower: [[Edm.String], fnFunction('lower')],
+
+  // https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_toupper
+  toupper: [[Edm.String], fnFunction('upper')],
+
+  // https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html#sec_trim
+  trim: [[Edm.String], fnFunction('trim')],
+};
+
+function processLiteral(token: Token): Date | boolean | number | string {
+  switch (token.value) {
+    case Edm.Boolean:
+      return token.raw === 'true';
+    case Edm.String:
+      return JSON.parse(`"${token.raw.slice(1, -1).replace(/"/g, '\\"')}"`);
+    case Edm.Byte:
+    case Edm.Decimal:
+    case Edm.Double:
+    case Edm.Int16:
+    case Edm.Int32:
+    case Edm.Int64:
+    case Edm.SByte:
+    case Edm.Single:
+      return Number(token.raw);
+    case Edm.Date:
+    case Edm.DateTimeOffset:
+      // The Date constructor will convert it to UTC.
+      return new Date(token.raw);
+    case Edm.Guid:
+      return token.raw;
+    case Edm.null:
+      return null;
+    default:
+      throw new TypeError(`${token.position}: Unhandled OData literal type: ${token.value}`);
+  }
+}
+
+function processName(token: Token, model: PartialModel, rename: Rename): Col | Json {
+  // OData uses `/` as a path separator, but Sequelize uses `.`.
+  // https://sequelize.org/master/manual/other-data-types.html#jsonb--postgresql-only-
+  const name = rename(token.raw).replace(/\//g, '.');
+  return name.includes('.') ? json(name) : col(`${model.tableName}.${name}`);
+}
+
+function processMethod(token: Token, model: PartialModel, rename: Rename): Fn | Where {
+  const { method, parameters } = token.value as { method: string; parameters: Token[] };
+
+  if (!has(functions, method)) {
+    throw new TypeError(`${token.position}: Filter function not implemented: ${method}`);
+  }
+  const [parameterTypes, implementation] = functions[token.value.method];
+
+  if (parameterTypes.length !== parameters.length) {
+    throw new TypeError(
+      `${token.position}: Expected ${parameterTypes.length} parameters, but got ${parameters.length}`,
+    );
+  }
+  const parsedParameters = parameters.map((parameter, index) => {
+    if (parameter.type === TokenType.FirstMemberExpression) {
+      return processName(parameter, model, rename);
+    }
+    if (parameter.type === TokenType.Literal) {
+      if (parameter.value !== parameterTypes[index]) {
+        throw new TypeError(
+          `${parameter.position}: Expected parameter of type ${parameterTypes[index]}, but got ${parameter.value}`,
+        );
+      }
+      return processLiteral(parameter);
+    }
+    if (parameter.type === 'MethodCallExpression') {
+      return processMethod(parameter, model, rename);
+    }
+    throw new TypeError(`${parameter.position}: Unhandled parameter type: ${parameter.type}`);
+  });
+
+  return implementation(...parsedParameters);
+}
+
+function processToken(
+  token: Token,
+  model: PartialModel,
+  rename: Rename,
+): WhereOptions | WhereValue {
+  if (token.type === 'FirstMemberExpression') {
+    return processName(token, model, rename) as WhereValue;
+  }
+  if (token.type === TokenType.MethodCallExpression) {
+    return processMethod(token, model, rename);
+  }
+  if (token.type === TokenType.ParenExpression) {
+    return processToken(token.value, model, rename);
+  }
+  if (operators.has(token.type)) {
+    return where(
+      processToken(token.value.left, model, rename),
+      operators.get(token.type),
+      processToken(token.value.right, model, rename),
+    );
+  }
+
+  if (token.type === TokenType.Literal) {
+    return processLiteral(token);
+  }
+  throw new TypeError(`${token.position}: Unhandled OData type: ${token.type}`);
+}
+
+/**
+ * Process OData logical expressions:
+ *
+ * - `and`
+ * - `or`
+ * - `not`
+ *
+ * @param token The token to process.
+ * @param model The model to do a query for.
+ * @param rename A rename function.
+ * @returns The Sequelize query that matches the given token.
+ */
+function processLogicalExpression(token: Token, model: PartialModel, rename: Rename): WhereOptions {
+  if (token.type === TokenType.BoolParenExpression || token.type === TokenType.CommonExpression) {
+    return processLogicalExpression(token.value, model, rename);
+  }
+
+  if (token.type === TokenType.NotExpression) {
+    return { [Op.not]: processLogicalExpression(token.value, model, rename) };
+  }
+
+  const op =
+    token.type === TokenType.AndExpression
+      ? Op.and
+      : token.type === TokenType.OrExpression
+      ? Op.o
