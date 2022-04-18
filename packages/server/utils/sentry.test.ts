617286b322c8bbac713b3a5a6990e575ba6236e5
plosys <89532245+plosys@users.noreply.github.com>
2022-04-18 12:54:53 +0000

    README formatting.

--- packages/server/utils/sentry.test.ts
+++ packages/server/utils/sentry.test.ts
@@ -40,4 +40,82 @@ describe('getSentryClientSettings', () => {
     expect(result).toStrictEqual({
       reportUri: 'https://sentry.io/api/42/security/?sentry_key=0123456789abcdef',
       sentryDsn: 'https://0123456789abcdef@sentry.io/42',
-      sentryEnvironmen
+      sentryEnvironment: 'test',
+      sentryOrigin: 'https://sentry.io',
+    });
+  });
+
+  it('should work with multiple allowed domains', () => {
+    setArgv({
+      sentryAllowedDomains: 'appsemble.app,*.test.appsemble.app',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+    });
+    const resultA = getSentryClientSettings('appsemble.app');
+    const resultB = getSentryClientSettings('testApp.test.appsemble.app');
+    const resultC = getSentryClientSettings('example.com');
+    const resultD = getSentryClientSettings('testApp.appsemble.appsemble.app');
+
+    expect(resultA).toStrictEqual({
+      reportUri: 'https://sentry.io/api/42/security/?sentry_key=0123456789abcdef',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+      sentryOrigin: 'https://sentry.io',
+    });
+    expect(resultB).toStrictEqual({
+      reportUri: 'https://sentry.io/api/42/security/?sentry_key=0123456789abcdef',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+      sentryOrigin: 'https://sentry.io',
+    });
+    expect(resultC).toStrictEqual({});
+    expect(resultD).toStrictEqual({});
+  });
+
+  it('should return an empty object if the domain isn’t allowed', () => {
+    setArgv({
+      sentryAllowedDomains: '*.foo.example',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+    });
+    const result = getSentryClientSettings('foo.bar.example');
+    expect(result).toStrictEqual({});
+  });
+
+  it('should ignore domain checks if a custom sentry DSN is used', () => {
+    setArgv({
+      sentryAllowedDomains: '*.foo.example',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+    });
+    const result = getSentryClientSettings(
+      'foo.bar.example',
+      'https://0123456789abcdef@sentry.io/43',
+      'test-2',
+    );
+    expect(result).toStrictEqual({
+      reportUri: 'https://sentry.io/api/43/security/?sentry_key=0123456789abcdef',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/43',
+      sentryEnvironment: 'test-2',
+      sentryOrigin: 'https://sentry.io',
+    });
+  });
+
+  it('should set the environment to undefined if a custom sentry DSN is used without a custom environment', () => {
+    setArgv({
+      sentryAllowedDomains: '*.foo.example',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/42',
+      sentryEnvironment: 'test',
+    });
+    const result = getSentryClientSettings(
+      'foo.bar.example',
+      'https://0123456789abcdef@sentry.io/43',
+    );
+    expect(result).toStrictEqual({
+      reportUri: 'https://sentry.io/api/43/security/?sentry_key=0123456789abcdef',
+      sentryDsn: 'https://0123456789abcdef@sentry.io/43',
+      sentryEnvironment: undefined,
+      sentryOrigin: 'https://sentry.io',
+    });
+  });
+});
---
+++ packages/server/utils/sentry.ts
@@ -0,0 +1,69 @@
+import { init } from '@sentry/node';
+import { matcher } from 'matcher';
+
+import pkg from '../package.json' assert { type: 'json' };
+import { argv } from './argv.js';
+
+interface SentrySettings {
+  /**
+   * The CORS `report-uri` to use.
+   */
+  reportUri?: string;
+
+  /**
+   * The Sentry origin to allow making requests to.
+   */
+  sentryOrigin?: string;
+
+  /**
+   * The configured Sentry DSN.
+   */
+  sentryDsn?: string;
+
+  /**
+   * The Sentry environment to use.
+   */
+  sentryEnvironment?: string;
+}
+
+/**
+ * Get client side Sentry settings to inject into the context for the given domain.
+ *
+ * @param domain The domain name to check.
+ * @param sentryDsn The custom Sentry DSN to use.
+ * @param sentryEnvironment The custom Sentry environment to use.
+ * @returns Sentry DSN and environment if it matches the `--sentry-allowed-domains` option.
+ */
+export function getSentryClientSettings(
+  domain: string,
+  sentryDsn?: string,
+  sentryEnvironment?: string,
+): SentrySettings {
+  if (
+    !sentryDsn &&
+    (!argv.sentryDsn ||
+      !argv.sentryAllowedDomains ||
+      !matcher(domain, argv.sentryAllowedDomains.split(',')).length)
+  ) {
+    return {};
+  }
+  const dsn = sentryDsn || argv.sentryDsn;
+  const { origin, pathname, username } = new URL(dsn);
+  return {
+    sentryDsn: dsn,
+    sentryEnvironment: sentryDsn
+      ? sentryEnvironment || undefined
+      : argv.sentryEnvironment || undefined,
+    sentryOrigin: origin,
+    reportUri: `${origin}/api${pathname}/security/?sentry_key=${username}`,
+  };
+}
+
+/**
+ * Setup Sentry server side.
+ */
+export function configureSentry(): void {
+  if (argv.sentryDsn) {
+    init({ dsn: argv.sentryDsn, environment: argv.sentryEnvironment, release: pkg.version });
+  }
+}
