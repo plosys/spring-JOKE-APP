98b3d233cf37443d8bceb7fc9820c5d974592c0d
plosys <89532245+plosys@users.noreply.github.com>
2022-06-04 07:40:49 +0000

    update Numbas runtime

---
+++ packages/studio/pages/apps/app/settings/index.tsx
@@ -0,0 +1,339 @@
+
+import {
+  Button,
+  CheckboxField,
+  Content,
+  FormButtons,
+  Message,
+  SelectField,
+  SimpleForm,
+  SimpleFormError,
+  SimpleFormField,
+  SimpleSubmit,
+  TextAreaField,
+  useConfirmation,
+  useMessages,
+  useMeta,
+} from '@appsemble/react-components';
+import { App, SSLStatus } from '@appsemble/types';
+import { domainPattern, googleAnalyticsIDPattern, normalize, toUpperCase } from '@appsemble/utils';
+import axios from 'axios';
+import { ReactElement, useMemo } from 'react';
+import { FormattedMessage, MessageDescriptor, useIntl } from 'react-intl';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+
+import { useSSLStatus } from '../../../../components/useSSLStatus.js';
+import { useApp } from '../index.js';
+import { IconTool } from './IconTool/index.js';
+import styles from './index.module.css';
+import { messages } from './messages.js';
+import { FormValues } from './types.js';
+
+function preprocessDomain(domain: string): string {
+  return domain
+    .trim()
+    .replace(/^https?:\/\//, '')
+    .split(/\./g)
+    .map((node) => normalize(node, false).slice(0, 63))
+    .join('.');
+}
+
+function getSSLMessage(status: SSLStatus): MessageDescriptor {
+  switch (status) {
+    case 'error':
+      return messages.sslError;
+    case 'missing':
+      return messages.sslMissing;
+    case 'pending':
+      return messages.sslPending;
+    case 'ready':
+      return messages.sslReady;
+    default:
+      return messages.sslUnknown;
+  }
+}
+
+/**
+ * Render the app settings view.
+ */
+export function SettingsPage(): ReactElement {
+  useMeta(messages.title);
+
+  const { app, setApp } = useApp();
+  const { formatMessage } = useIntl();
+
+  const push = useMessages();
+  const navigate = useNavigate();
+  const { lang } = useParams<{ lang: string }>();
+
+  const pathDomain = `${app.path}.${app.OrganizationId}.${window.location.hostname}`;
+  const domains = [pathDomain];
+  if (app.domain) {
+    domains.push(app.domain);
+  }
+
+  const sslStatus = useSSLStatus(...domains);
+
+  // This is needed, because the app domain may be null.
+  const defaultValues = useMemo<FormValues>(
+    () => ({
+      emailName: app.emailName || '',
+      maskableIcon: null,
+      domain: app.domain || '',
+      googleAnalyticsID: app.googleAnalyticsID || '',
+      sentryDsn: app.sentryDsn || '',
+      sentryEnvironment: app.sentryEnvironment || '',
+      icon: null,
+      iconBackground: app.iconBackground,
+      path: app.path,
+      visibility: app.visibility,
+      locked: app.locked,
+      longDescription: app.longDescription || '',
+      showAppDefinition: app.showAppDefinition,
+    }),
+    [app],
+  );
+
+  const onSubmit = async (values: FormValues): Promise<void> => {
+    const form = new FormData();
+    form.set('emailName', values.emailName);
+    form.set('domain', values.domain);
+    form.set('googleAnalyticsID', values.googleAnalyticsID);
+    form.set('sentryDsn', values.sentryDsn);
+    form.set('sentryEnvironment', values.sentryEnvironment);
+    form.set('path', values.path);
+    form.set('visibility', values.visibility);
+    form.set('iconBackground', values.iconBackground);
+    form.set('longDescription', values.longDescription);
+    form.set('showAppDefinition', String(values.showAppDefinition));
+    if (values.icon !== app.iconUrl) {
+      form.set('icon', values.icon);
+    }
+    if (values.maskableIcon) {
+      form.set('maskableIcon', values.maskableIcon);
+    }
+
+    const { data } = await axios.patch<App>(`/api/apps/${app.id}`, form);
+    push({ color: 'success', body: formatMessage(messages.updateSuccess) });
+    setApp(data);
+  };
+
+  const onDelete = useConfirmation({
+    title: <FormattedMessage {...messages.deleteWarningTitle} />,
+    body: <FormattedMessage {...messages.deleteWarning} />,
+    cancelLabel: <FormattedMessage {...messages.cancel} />,
+    confirmLabel: <FormattedMessage {...messages.delete} />,
+    async action() {
+      const { id } = app;
+
+      try {
+        await axios.delete(`/api/apps/${id}`);
+        push({
+          body: formatMessage(messages.deleteSuccess, {
+            name: app.definition.name,
+          }),
+          color: 'info',
+        });
+        navigate('/apps');
+      } catch {
+        push(formatMessage(messages.errorDelete));
+      }
+    },
+  });
+
+  const onToggleLock = useConfirmation({
+    title: <FormattedMessage {...(app.locked ? messages.unlockApp : messages.lockApp)} />,
+    body: (
+      <FormattedMessage
+        {...(app.locked ? messages.unlockAppDescription : messages.lockAppDescription)}
+      />
+    ),
+    cancelLabel: <FormattedMessage {...messages.cancel} />,
+    confirmLabel: <FormattedMessage {...(app.locked ? messages.unlockApp : messages.lockApp)} />,
+    color: 'warning',
+    async action() {
+      const { id, locked } = app;
+      try {
+        await axios.post(`/api/apps/${id}/lock`, { locked: !locked });
+        setApp({ ...app, locked: !locked });
+        push({
+          body: formatMessage(locked ? messages.unlockedSuccessfully : messages.lockedSuccessfully),
+          color: 'info',
+        });
+      } catch {
+        push(formatMessage(messages.lockError));
+      }
+    },
+  });
+
+  return (
+    <>
+      <Content fullwidth>
+        <SimpleForm defaultValues={defaultValues} onSubmit={onSubmit}>
+          <SimpleFormError>{() => <FormattedMessage {...messages.updateError} />}</SimpleFormError>
+          <IconTool disabled={app.locked} />
+          <SimpleFormField
+            component={TextAreaField}
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.longDescriptionDescription} />}
+            label={<FormattedMessage {...messages.longDescription} />}
+            name="longDescription"
+          />
+          <SimpleFormField
+            component={SelectField}
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.visibilityDescription} />}
+            label={<FormattedMessage {...messages.visibilityLabel} />}
+            name="visibility"
+          >
+            <option value="public">{formatMessage(messages.public)}</option>
+            <option value="unlisted">{formatMessage(messages.unlisted)}</option>
+            <option value="private">{formatMessage(messages.private)}</option>
+          </SimpleFormField>
+          <SimpleFormField
+            component={CheckboxField}
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.showAppDefinitionDescription} />}
+            label={<FormattedMessage {...messages.showAppDefinitionLabel} />}
+            name="showAppDefinition"
+          />
+          <SimpleFormField
+            addonLeft={
+              <Button
+                className={`is-light ${
+                  sslStatus
+                    ? sslStatus[pathDomain] === 'ready'
+                      ? 'is-success'
+                      : 'is-danger'
+                    : 'is-loading'
+                }`}
+                component="label"
+                htmlFor="path"
+                title={formatMessage(getSSLMessage(sslStatus?.[pathDomain]))}
+              >
+                {`${window.location.protocol}//`}
+              </Button>
+            }
+            addonRight={
+              <Button className="is-light" component="label" htmlFor="path">
+                {`.${app.OrganizationId}.${window.location.host}`}
+              </Button>
+            }
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.pathDescription} />}
+            label={<FormattedMessage {...messages.path} />}
+            maxLength={30}
+            name="path"
+            placeholder={normalize(app.definition.name)}
+            preprocess={(value) => normalize(value)}
+            required
+          />
+          <SimpleFormField
+            addonLeft={
+              <Button
+                className={`is-light ${
+                  sslStatus
+                    ? sslStatus[app.domain] === 'ready'
+                      ? 'is-success'
+                      : 'is-danger'
+                    : 'is-loading'
+                }`}
+                component="label"
+                htmlFor="domain"
+                title={formatMessage(getSSLMessage(sslStatus?.[app.domain]))}
+              >
+                {`${window.location.protocol}//`}
+              </Button>
+            }
+            disabled={app.locked}
+            help={
+              <FormattedMessage
+                {...messages.domainDescription}
+                values={{
+                  link: (link) => (
+                    <Link rel="noopener noreferrer" target="_blank" to={`/${lang}/docs/guide/dns`}>
+                      {link}
+                    </Link>
+                  ),
+                }}
+              />
+            }
+            label={<FormattedMessage {...messages.domain} />}
+            maxLength={253}
+            name="domain"
+            pattern={domainPattern}
+            preprocess={preprocessDomain}
+            validityMessages={{
+              patternMismatch: <FormattedMessage {...messages.domainError} />,
+            }}
+          />
+          <SimpleFormField
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.emailNameDescription} />}
+            label={<FormattedMessage {...messages.emailNameLabel} />}
+            maxLength={30}
+            name="emailName"
+            placeholder="Appsemble"
+          />
+          <SimpleFormField
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.googleAnalyticsIDDescription} />}
+            label={<FormattedMessage {...messages.googleAnalyticsIDLabel} />}
+            maxLength={15}
+            name="googleAnalyticsID"
+            pattern={googleAnalyticsIDPattern}
+            preprocess={toUpperCase}
+            validityMessages={{
+              patternMismatch: <FormattedMessage {...messages.googleAnalyticsError} />,
+            }}
+          />
+          <SimpleFormField
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.sentryDsnDescription} />}
+            label={<FormattedMessage {...messages.sentryDsnLabel} />}
+            name="sentryDsn"
+            type="url"
+          />
+          <SimpleFormField
+            disabled={app.locked}
+            help={<FormattedMessage {...messages.sentryEnvironmentDescription} />}
+            label={<FormattedMessage {...messages.sentryEnvironmentLabel} />}
+            name="sentryEnvironment"
+          />
+          <FormButtons>
+            <SimpleSubmit color="primary" disabled={app.locked} type="submit">
+              <FormattedMessage {...messages.saveChanges} />
+            </SimpleSubmit>
+          </FormButtons>
+        </SimpleForm>
+      </Content>
+      <hr />
+      <Content>
+        <Message
+          className={styles.appLock}
+          color="warning"
+          header={<FormattedMessage {...messages.appLock} />}
+        >
+          <p className="content">
+            <FormattedMessage {...messages.lockedDescription} />
+          </p>
+          <Button color="warning" icon={app.locked ? 'unlock' : 'lock'} onClick={onToggleLock}>
+            <FormattedMessage {...(app.locked ? messages.unlockApp : messages.lockApp)} />
+          </Button>
+        </Message>
+        <Message
+          className={styles.dangerZone}
+          color="danger"
+          header={<FormattedMessage {...messages.dangerZone} />}
+        >
+          <p className="content">
+            <FormattedMessage {...messages.deleteHelp} />
+          </p>
+          <Button color="danger" disabled={app.locked} icon="trash-alt" onClick={onDelete}>
+            <FormattedMessage {...messages.delete} />
+          </Button>
+        </Message>
+      </Content>
+    </>
+  );
+}
---
+++ packages/studio/pages/apps/app/settings/messages.ts
@@ -0,0 +1,58 @@
+import { defineMessages } from 'react-intl';
+
+export const messages = defineMessages({
+  title: 'Settings',
+  chooseFile: 'Choose file…',
+  noFile: 'No file chosen',
+  icon: 'Icon',
+  iconDescription: 'The icon that will be shown to the user.',
+  visibilityLabel: 'Visibility',
+  visibilityDescription:
+    'Public apps are available in the app store. Unlisted apps aren’t listed publicly, but they are accessible using a direct link. Private apps are only visible to organization members.',
+  public: 'Public',
+  unlisted: 'Unlisted',
+  private: 'Private',
+  showAppDefinitionDescription: 'If checked, other people may view the app definition of this app.',
+  showAppDefinitionLabel: 'Show app definition',
+  appLock: 'App lock',
+  lockApp: 'Lock app',
+  unlockApp: 'Unlock app',
+  lockedDescription:
+    'If the app is locked, it is prevented from being updated without unlocking it first.',
+  unlockAppDescription: 'Are you sure you want to unlock this app?',
+  lockAppDescription: 'Are you sure you want to lock this app?',
+  lockedSuccessfully: 'Successfully locked the app.',
+  unlockedSuccessfully: 'Successfully unlocked the app.',
+  lockError: 'Something went wrong when trying to change the locked status of the app.',
+  path: 'Path',
+  pathDescription: 'The path used to access the app.',
+  domain: 'Domain name',
+  domainDescription:
+    'The domain name on which this app is available. For more information on how to set this up, please refer to the <link>documentation</link>.',
+  domainError: 'This must be a valid domain name without a protocol',
+  googleAnalyticsIDLabel: 'Google Analytics ID',
+  googleAnalyticsIDDescription:
+    'If this is specified, Google analytics will be applied to your app.',
+  googleAnalyticsError: 'This needs to be a valid Google Analytics ID',
+  saveChanges: 'Save changes',
+  updateSuccess: 'Successfully updated settings.',
+  updateError: 'Something went wrong when trying to update the settings.',
+  deleteSuccess: 'Successfully deleted app {name}',
+  delete: 'Delete app',
+  deleteHelp: 'Deleting an app cannot be undone.',
+  deleteWarningTitle: 'Deleting app',
+  deleteWarning: 'Are you sure you want to delete this app? This action cannot be reverted.',
+  errorDelete: 'Something went wrong trying to delete this app',
+  cancel: 'Cancel',
+  dangerZone: 'Dangerous actions',
+  longDescription: 'Long description',
+  longDescriptionDescription:
+    'This field can be used to describe the app in more detail than the app’s description. Supports Markdown syntax.',
+  sentryDsnLabel: 'Sentry DSN',
+  sentryDsnDescription:
+    'If this is specified, Sentry monitoring and error tracking will be applied to your app.',
+  sentryEnvironmentLabel: 'Sentry environment',
+  sentryEnvironmentDescription:
+    'The environment that should be passed to Sentry when using a custom Sentry DSN.',
+  sslError: 'There was a problem generating an SSL certificate for this domain',
+  sslUnknown: 'The current status of the SSL certifica
