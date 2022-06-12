60cf0bef2dc7907d44a5aac4661b78923d5e5ce9
plosys <89532245+plosys@users.noreply.github.com>
2022-06-12 09:40:39 +0000

    Redundant

--- packages/studio/pages/organizations/organization/index.tsx
+++ packages/studio/pages/organizations/organization/index.tsx
@@ -26,4 +26,74 @@ import { SettingsPage } from './SettingsPage/index.js';
 export function OrganizationRoutes(): ReactElement {
   const { organizations } = useUser();
   const { lang, organizationId } = useParams<{ lang: string; organizationId: string }>();
-  const 
+  const url = `/${lang}/organizations/${organizationId}`;
+
+  const result = useData<Organization>(`/api/organizations/${organizationId}`);
+  const userOrganization = organizations?.find((org) => org.id === organizationId);
+  const mayEdit = userOrganization && checkRole(userOrganization.role, Permission.EditOrganization);
+
+  useSideMenu(
+    result.data && (
+      <MenuSection label={<span className="ml-2">{result.data.name}</span>}>
+        <MenuItem exact icon="briefcase" to={url}>
+          <FormattedMessage {...messages.organization} />
+        </MenuItem>
+        {userOrganization ? (
+          <MenuItem exact icon="users" to={`${url}/members`}>
+            <FormattedMessage {...messages.members} />
+          </MenuItem>
+        ) : null}
+        {mayEdit ? (
+          <MenuItem exact icon="cog" to={`${url}/settings`}>
+            <FormattedMessage {...messages.settings} />
+          </MenuItem>
+        ) : null}
+      </MenuSection>
+    ),
+  );
+
+  if (!normalized.test(organizationId)) {
+    return <Navigate to={String(url.replace(organizationId, normalize(organizationId)))} />;
+  }
+
+  return (
+    <AsyncDataView
+      errorMessage={<FormattedMessage {...messages.error} />}
+      loadingMessage={<FormattedMessage {...messages.loading} />}
+      result={result}
+    >
+      {(organization) => (
+        <MetaSwitch title={organization.name || organizationId}>
+          <Route element={<IndexPage organization={userOrganization ?? organization} />} path="/" />
+
+          <Route
+            element={
+              <ProtectedRoute
+                organization={userOrganization}
+                permission={Permission.EditOrganization}
+              />
+            }
+          >
+            <Route
+              element={
+                <SettingsPage
+                  onChangeOrganization={result.setData}
+                  organization={userOrganization ?? organization}
+                />
+              }
+              path="/settings"
+            />
+          </Route>
+
+          {userOrganization ? (
+            <Route element={<ProtectedRoute organization={userOrganization} />}>
+              <Route element={<MembersPage />} path="/members" />
+            </Route>
+          ) : null}
+
+          <Route element={<Navigate to={url} />} path="*" />
+        </MetaSwitch>
+      )}
+    </AsyncDataView>
+  );
+}
---
+++ packages/studio/pages/privacy/index.tsx
@@ -0,0 +1,12 @@
+
+import { useMeta } from '@appsemble/react-components';
+import { ReactElement } from 'react';
+
+import { messages } from './messages.js';
+import PrivacyPolicy from './privacy-policy.md';
+
+export function PrivacyPolicyPage(): ReactElement {
+  useMeta(messages.title, messages.description);
+
+  return <PrivacyPolicy />;
+}
