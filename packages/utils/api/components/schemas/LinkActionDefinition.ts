2d35c559a5a7aa795bcff263938adb694eaf5fa1
plosys <89532245+plosys@users.noreply.github.com>
2022-07-19 15:08:29 +0000

    title edit

---
+++ packages/utils/api/components/schemas/LinkActionDefinition.ts
@@ -0,0 +1,29 @@
+
+import { BaseActionDefinition } from './BaseActionDefinition.js';
+import { extendJSONSchema } from './utils.js';
+
+export const LinkActionDefinition = extendJSONSchema(BaseActionDefinition, {
+  type: 'object',
+  additionalProperties: false,
+  required: ['type', 'to'],
+  properties: {
+    type: {
+      enum: ['link'],
+      description:
+        'The link action can be used to redirect the user to other pages or absolute URLs.',
+    },
+    to: {
+      description:
+        'The name of the page to link to. Subpages can be referred to using arrays. If this matches with an absolute URL, link will open this instead of matching it with a page or subpage.',
+      anyOf: [
+        {
+          type: 'string',
+        },
+        {
+          type: 'array',
+          items: { type: 'string' },
+        },
+      ],
+    },
+  },
+});
---
+++ packages/utils/api/components/schemas/OAuth2ClientCredentials.ts
@@ -0,0 +1,37 @@
+import { OpenAPIV3 } from 'openapi-types';
+
+import { scopes } from '../../../constants/index.js';
+
+export const OAuth2ClientCredentials: OpenAPIV3.NonArraySchemaObject = {
+  type: 'object',
+  description: 'OAuth2 client credentials',
+  required: ['description', 'scopes'],
+  additionalProperties: false,
+  properties: {
+    id: {
+      type: 'string',
+      description: 'The generated client id',
+      readOnly: true,
+    },
+    secret: {
+      type: 'string',
+      description: 'The generated client secret',
+      readOnly: true,
+    },
+    description: {
+      type: 'string',
+      description: 'A description for the user to recognize the client.',
+      maxLength: 50,
+    },
+    expires: {
+      type: 'string',
+      description: 'When the client credentials expire automatically.',
+      format: 'date-time',
+    },
+    scopes: {
+      type: 'array',
+      description: 'The scopes that have been granted to the client.',
+      items: {
+        enum: [...scopes],
+      },
+  
