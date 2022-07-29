daa2ea72ca2adef2d31f8894c7d914785b4ff8c5
plosys <89532245+plosys@users.noreply.github.com>
2022-07-29 08:36:41 +0000

    Minor readme change

--- packages/utils/api/paths/apps.ts
+++ packages/utils/api/paths/apps.ts
@@ -42,4 +42,145 @@ export const paths: OpenAPIV3.PathsObject = {
                 yaml: {
                   type: 'string',
                   description: 'The original YAML definition used to define the app.',
-                
+                },
+                OrganizationId: {
+                  $ref: '#/components/schemas/Organization/properties/id',
+                },
+                icon: {
+                  type: 'string',
+                  format: 'binary',
+                  description: 'The app icon.',
+                },
+                maskableIcon: {
+                  type: 'string',
+                  format: 'binary',
+                  description: 'The app icon.',
+                },
+                iconBackground: {
+                  type: 'string',
+                  pattern: hexColor.source,
+                  description: 'The background color to use for the maskable icon.',
+                },
+                coreStyle: {
+                  type: 'string',
+                  description: 'The custom style to apply to the core app.',
+                },
+                sharedStyle: {
+                  type: 'string',
+                  description: 'The custom style to apply to all parts of app.',
+                },
+                screenshots: {
+                  type: 'array',
+                  description: 'Screenshots to showcase in the store',
+                  items: {
+                    type: 'string',
+                    format: 'binary',
+                  },
+                },
+              },
+            },
+            encoding: {
+              coreStyle: { contentType: 'text/css' },
+              sharedStyle: { contentType: 'text/css' },
+              icon: {
+                contentType: 'image/png,image/jpeg,image/tiff,image/webp',
+              },
+              screenshots: {
+                contentType: 'image/png,image/jpeg,image/tiff,image/webp',
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        201: {
+          description: 'The app that was created.',
+          $ref: '#/components/responses/app',
+        },
+      },
+      security: [{ studio: [] }, { cli: ['apps:write'] }],
+    },
+    get: {
+      tags: ['app'],
+      parameters: [
+        {
+          name: 'language',
+          schema: { type: 'string' },
+          description: 'The language to include the translations of, if available',
+          in: 'query',
+        },
+      ],
+      description: 'Get all existing apps.',
+      operationId: 'queryApps',
+      responses: {
+        200: {
+          description: 'The list of all apps.',
+          content: {
+            'application/json': {
+              schema: {
+                type: 'array',
+                items: {
+                  $ref: '#/components/schemas/App',
+                },
+              },
+            },
+          },
+        },
+      },
+      security: [{ studio: [] }, {}],
+    },
+  },
+  '/api/apps/{appId}': {
+    parameters: [{ $ref: '#/components/parameters/appId' }],
+    get: {
+      tags: ['app'],
+      parameters: [
+        {
+          name: 'language',
+          schema: { type: 'string' },
+          description: 'The language to include the translations of, if available',
+          in: 'query',
+        },
+      ],
+      description: 'Get a single app',
+      operationId: 'getAppById',
+      responses: {
+        200: {
+          description: 'The app that matches the given id.',
+          $ref: '#/components/responses/app',
+        },
+      },
+      security: [{ studio: [] }, {}],
+    },
+    patch: {
+      tags: ['app'],
+      description: 'Update parts of an existing app',
+      operationId: 'patchApp',
+      requestBody: {
+        content: {
+          'multipart/form-data': {
+            schema: {
+              type: 'object',
+              properties: {
+                domain: {
+                  $ref: '#/components/schemas/App/properties/domain',
+                },
+                path: {
+                  $ref: '#/components/schemas/App/properties/path',
+                },
+                visibility: {
+                  $ref: '#/components/schemas/App/properties/visibility',
+                },
+                template: {
+                  $ref: '#/components/schemas/App/properties/template',
+                },
+                longDescription: {
+                  $ref: '#/components/schemas/App/properties/longDescription',
+                },
+                force: {
+                  type: 'boolean',
+                  description: 'Whether the locked property should be ignored.',
+                },
+                yaml: {
+                  type: 'string',
+                  description: 'The or
