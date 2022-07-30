926c4283d668259e9cd473ceff29f4c31d547bc0
plosys <89532245+plosys@users.noreply.github.com>
2022-07-30 08:22:56 +0000

    msm: kgsl: Remove extra interrupts when setting MMU state

--- packages/utils/api/paths/apps.ts
+++ packages/utils/api/paths/apps.ts
@@ -183,4 +183,140 @@ export const paths: OpenAPIV3.PathsObject = {
                 },
                 yaml: {
                   type: 'string',
-                  description: 'The or
+                  description: 'The original YAML definition used to define the app.',
+                },
+                icon: {
+                  type: 'string',
+                  format: 'binary',
+                  description: 'The app icon.',
+                },
+                maskableIcon: {
+                  type: 'string',
+                  format: 'binary',
+                  description: 'The app icon.',
+                },
+                iconBackground: {
+                  type: 'string',
+                  pattern: hexColor.source,
+                },
+                coreStyle: {
+                  type: 'string',
+                  description: 'The custom style to apply to the core app.',
+                },
+                sharedStyle: {
+                  type: 'string',
+                  description: 'The custom style to apply to all parts of app.',
+                },
+                screenshots: {
+                  type: 'array',
+                  description: 'Screenshots to showcase in the store',
+                  items: {
+                    type: 'string',
+                    format: 'binary',
+                  },
+                },
+                showAppsembleLogin: {
+                  type: 'boolean',
+                  description: 'Whether the Appsemble login method should be shown.',
+                },
+                showAppsembleOAuth2Login: {
+                  type: 'boolean',
+                  description: 'Whether the Appsemble OAuth2 login method should be shown.',
+                },
+                emailName: {
+                  type: 'string',
+                  description: 'The name used for emails.',
+                },
+                emailHost: {
+                  type: 'string',
+                  description: 'The hostname of the SMTP server.',
+                },
+                emailPassword: {
+                  type: 'string',
+                  description:
+                    'The password to use for SMTP authentication. This gets encrypted when stored.',
+                },
+                emailUser: {
+                  type: 'string',
+                  description: 'The username used to authenticate against the SMTP server.',
+                },
+                emailPort: {
+                  type: 'string',
+                  description: 'The port used for the SMTP server.',
+                },
+                emailSecure: {
+                  type: 'boolean',
+                  description: 'Whether TLS is being used.',
+                },
+              },
+            },
+            encoding: {
+              coreStyle: { contentType: 'text/css' },
+              sharedStyle: { contentType: 'text/css' },
+              icon: {
+                contentType: 'image/png,image/jpeg,image/tiff,image/webp',
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        200: {
+          description: 'The updated app.',
+          $ref: '#/components/responses/app',
+        },
+      },
+      security: [{ studio: [] }, { cli: ['apps:write'] }],
+    },
+    delete: {
+      tags: ['app'],
+      description: 'Delete an existing app',
+      operationId: 'deleteApp',
+      responses: {
+        204: {
+          description: 'The app was successfully deleted.',
+        },
+      },
+      security: [{ studio: [] }],
+    },
+  },
+  '/api/apps/{appId}/lock': {
+    parameters: [{ $ref: '#/components/parameters/appId' }],
+    post: {
+      tags: ['app'],
+      description: 'Update the locked property an app.',
+      operationId: 'setAppLock',
+      requestBody: {
+        required: true,
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              required: ['locked'],
+              properties: {
+                locked: {
+                  $ref: '#/components/schemas/App/properties/locked',
+                  description: 'Whether the app should be locked.',
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        204: {
+          description: 'Lock status successfully changed',
+        },
+      },
+      security: [{ studio: [] }, { cli: ['apps:write'] }],
+    },
+  },
+  '/api/apps/{appId}/email': {
+    parameters: [{ $ref: '#/components/parameters/appId' }],
+    get: {
+      tags: ['app'],
+      description: 'Get the app’s email settings.',
+      operationId: 'getAppEmailSettings',
+      responses: {
+        200: {
+          description: 'The current app email 
