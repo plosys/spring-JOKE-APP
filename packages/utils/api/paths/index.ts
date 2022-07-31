6ada63ef29a6e16c418f3fbf8ea5f577acec5cda
plosys <89532245+plosys@users.noreply.github.com>
2022-07-31 19:35:17 +0000

    [UPD]

---
+++ packages/utils/api/paths/index.ts
@@ -0,0 +1,6 @@
+import { paths as action } from './action.js';
+import { paths as translations } from './appMessages.js';
+import { paths as appOAuth2Secrets } from './appOAuth2Secrets.js';
+import { paths as apps } from './apps.js';
+import { paths as appSamlSecrets } from './appSamlSecrets.js';
+im
