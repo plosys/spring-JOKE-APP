b3b81d6fb9851faf6c1bda241f4331373cb5ea53
plosys <89532245+plosys@users.noreply.github.com>
2022-08-10 15:49:27 +0000

    remove unused scripts

--- packages/utils/api/paths/templates.ts
+++ packages/utils/api/paths/templates.ts
@@ -28,4 +28,58 @@ export const paths: OpenAPIV3.PathsObject = {
                       type: 'boolean',
                       description: 'Whether this template supports pre-made resources',
                     },
-                 
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      security: [{ studio: [] }],
+    },
+    post: {
+      tags: ['template'],
+      description: 'Register a new app using a template.',
+      operationId: 'createTemplateApp',
+      requestBody: {
+        description: 'The template to use for app creation.',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              required: ['templateId', 'organizationId'],
+              properties: {
+                templateId: {
+                  type: 'number',
+                  description: 'The ID of the template.',
+                },
+                name: {
+                  $ref: '#/components/schemas/AppDefinition/properties/name',
+                },
+                description: {
+                  $ref: '#/components/schemas/AppDefinition/properties/description',
+                },
+                organizationId: {
+                  $ref: '#/components/schemas/Organization/properties/id',
+                },
+                resources: {
+                  type: 'boolean',
+                  description: 'Include example resources.',
+                },
+                visibility: {
+                  $ref: '#/components/schemas/App/properties/visibility',
+                },
+              },
+            },
+          },
+        },
+      },
+      responses: {
+        201: {
+          $ref: '#/components/responses/app',
+        },
+      },
+      security: [{ studio: [] }],
+    },
+  },
+};
