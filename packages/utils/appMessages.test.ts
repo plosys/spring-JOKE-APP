44cab593eb6c439098dfa59de5d1d392a1822bfa
plosys <89532245+plosys@users.noreply.github.com>
2022-08-13 08:39:13 +0000

    Apply codestyle

--- packages/utils/appMessages.test.ts
+++ packages/utils/appMessages.test.ts
@@ -81,4 +81,181 @@ describe('extractAppMessages', () => {
         },
       ],
     });
-    expect(result).toMatchObject({ messageIds: { foo: '' }, app: { 'pages.page
+    expect(result).toMatchObject({ messageIds: { foo: '' }, app: { 'pages.page': 'Page' } });
+  });
+
+  it('should extract remappers from block parameters', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              parameters: { foo: { 'string.format': { messageId: 'foo' } } },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({ messageIds: { foo: '' }, app: { 'pages.page': 'Page' } });
+  });
+
+  it('should extract action remap', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              actions: {
+                onClick: {
+                  type: 'noop',
+                  remap: { 'string.format': { messageId: 'onClickMessageId' } },
+                  onError: {
+                    type: 'noop',
+                    remap: { 'string.format': { messageId: 'onErrorMessageId' } },
+                  },
+                  onSuccess: {
+                    type: 'noop',
+                    remap: { 'string.format': { messageId: 'onSuccessMessageId' } },
+                  },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({
+      messageIds: { onClickMessageId: '', onErrorMessageId: '', onSuccessMessageId: '' },
+      app: { 'pages.page': 'Page' },
+    });
+  });
+
+  it('should extract condition action if', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              actions: {
+                onClick: {
+                  type: 'condition',
+                  if: { translate: 'ifCondition' },
+                  then: { type: 'noop' },
+                  else: { type: 'noop' },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({
+      messageIds: { ifCondition: '' },
+      app: { 'pages.page': 'Page' },
+    });
+  });
+
+  it('should extract dialog action title', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              actions: {
+                onClick: {
+                  type: 'dialog',
+                  title: { translate: 'dialogTitle' },
+                  blocks: [],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({
+      messageIds: { dialogTitle: '' },
+      app: { 'pages.page': 'Page' },
+    });
+  });
+
+  it('should extract email action remappers', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              actions: {
+                onClick: {
+                  type: 'email',
+                  attachments: { translate: 'attachments' },
+                  bcc: { translate: 'bcc' },
+                  body: { translate: 'body' },
+                  cc: { translate: 'cc' },
+                  subject: { translate: 'subject' },
+                  to: { translate: 'to' },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({
+      messageIds: { attachments: '', bcc: '', body: '', cc: '', subject: '', to: '' },
+      app: { 'pages.page': 'Page' },
+    });
+  });
+
+  it('should extract flow.to action step', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+        {
+          name: 'Page',
+          blocks: [
+            {
+              type: 'test',
+              version: '1.2.3',
+              actions: {
+                onClick: {
+                  type: 'flow.to',
+                  step: { translate: 'toPage' },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    });
+    expect(result).toMatchObject({
+      messageIds: { toPage: '' },
+      app: { 'pages.page': 'Page' },
+    });
+  });
+
+  it('should extract message action body', () => {
+    const result = extractAppMessages({
+      defaultPage: '',
+      pages: [
+   
