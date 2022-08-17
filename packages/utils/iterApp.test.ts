f628eaa7cd22a221c57b458fd29f92db9da6887b
plosys <89532245+plosys@users.noreply.github.com>
2022-08-17 22:13:12 +0000

    key

--- packages/utils/iterApp.test.ts
+++ packages/utils/iterApp.test.ts
@@ -165,4 +165,182 @@ describe('iterAction', () => {
       onBlockList,
     });
 
-    expect(onAction).toHaveBeenCalledWith(ac
+    expect(onAction).toHaveBeenCalledWith(action, []);
+    expect(onBlockList).not.toHaveBeenCalled();
+    expect(result).toBe(false);
+  });
+});
+
+describe('iterBlock', () => {
+  it('should call the appropriate callbacks', () => {
+    const onAction = import.meta.jest.fn();
+    const onBlock = import.meta.jest.fn();
+
+    const block: BlockDefinition = {
+      type: 'list',
+      version: '1.2.3',
+      actions: {
+        onClick: {
+          type: 'log',
+        },
+      },
+    };
+
+    const result = iterBlock(block, { onAction, onBlock });
+
+    expect(onAction).toHaveBeenCalledWith(block.actions.onClick, ['actions', 'onClick']);
+    expect(onBlock).toHaveBeenCalledWith(block, []);
+    expect(result).toBe(false);
+  });
+
+  it('should abort if onBlock returns true', () => {
+    const onAction = import.meta.jest.fn();
+    const onBlock = import.meta.jest.fn().mockReturnValue(true);
+
+    const block: BlockDefinition = {
+      type: 'list',
+      version: '1.2.3',
+      actions: {
+        onClick: {
+          type: 'log',
+        },
+      },
+    };
+
+    const result = iterBlock(block, { onAction, onBlock });
+
+    expect(onAction).not.toHaveBeenCalled();
+    expect(onBlock).toHaveBeenCalledWith(block, []);
+    expect(result).toBe(true);
+  });
+
+  it('should return the return value of onAction', () => {
+    const onAction = import.meta.jest.fn().mockReturnValue(true);
+    const onBlock = import.meta.jest.fn();
+
+    const block: BlockDefinition = {
+      type: 'list',
+      version: '1.2.3',
+      actions: {
+        onClick: {
+          type: 'log',
+        },
+      },
+    };
+
+    const result = iterBlock(block, { onAction, onBlock });
+
+    expect(onAction).toHaveBeenCalledWith(block.actions.onClick, ['actions', 'onClick']);
+    expect(onBlock).toHaveBeenCalledWith(block, []);
+    expect(result).toBe(true);
+  });
+});
+
+describe('iterBlockList', () => {
+  it('should call the appropriate callbacks', () => {
+    const onBlock = import.meta.jest.fn();
+    const onBlockList = import.meta.jest.fn();
+
+    const blocks: BlockDefinition[] = [
+      {
+        type: 'list',
+        version: '1.2.3',
+        actions: {
+          onClick: {
+            type: 'log',
+          },
+        },
+      },
+    ];
+
+    const result = iterBlockList(blocks, { onBlockList, onBlock });
+
+    expect(onBlockList).toHaveBeenCalledWith(blocks, []);
+    expect(onBlock).toHaveBeenCalledWith(blocks[0], [0]);
+    expect(result).toBe(false);
+  });
+
+  it('should abort if onBlockList returns true', () => {
+    const onBlock = import.meta.jest.fn();
+    const onBlockList = import.meta.jest.fn().mockReturnValue(true);
+
+    const blocks: BlockDefinition[] = [
+      {
+        type: 'list',
+        version: '1.2.3',
+        actions: {
+          onClick: {
+            type: 'log',
+          },
+        },
+      },
+    ];
+
+    const result = iterBlockList(blocks, { onBlockList, onBlock });
+
+    expect(onBlockList).toHaveBeenCalledWith(blocks, []);
+    expect(onBlock).not.toHaveBeenCalled();
+    expect(result).toBe(true);
+  });
+});
+
+describe('iterPage', () => {
+  it('should iterate over a page', () => {
+    const onBlockList = import.meta.jest.fn();
+    const onPage = import.meta.jest.fn();
+
+    const page: PageDefinition = {
+      name: 'Page',
+      blocks: [],
+    };
+
+    const result = iterPage(page, { onBlockList, onPage });
+
+    expect(onBlockList).toHaveBeenCalledWith(page.blocks, ['blocks']);
+    expect(onPage).toHaveBeenCalledWith(page, []);
+    expect(result).toBe(false);
+  });
+
+  it('should abort if onPage returns true', () => {
+    const onBlockList = import.meta.jest.fn();
+    const onPage = import.meta.jest.fn().mockReturnValue(true);
+
+    const page: PageDefinition = {
+      name: 'Page',
+      blocks: [],
+    };
+
+    const result = iterPage(page, { onBlockList, onPage });
+
+    expect(onBlockList).not.toHaveBeenCalled();
+    expect(onPage).toHaveBeenCalledWith(page, []);
+    expect(result).toBe(true);
+  });
+
+  it('should iterate a flow page', () => {
+    const onBlockList = import.meta.jest.fn();
+    const onPage = import.meta.jest.fn();
+
+    const page: PageDefinition = {
+      name: 'Page',
+      type: 'flow',
+      steps: [
+        {
+          name: 'Flow page 1',
+          blocks: [],
+        },
+      ],
+    };
+
+    const result = iterPage(page, { onBlockList, onPage });
+
+    expect(onBlockList).toHaveBeenCalledWith(page.steps[0].blocks, ['steps', 0, 'blocks']);
+    expect(onPage).toHaveBeenCalledWith(page, []);
+    expect(result).toBe(false);
+  });
+
+  it('should iterate a tabs page', () => {
+    const onBlockList = import.meta.jest.fn();
+    const onPage = import.meta.jest.fn();
+
+    const pa
