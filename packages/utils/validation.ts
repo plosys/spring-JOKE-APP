d3ef6124c1419d3cd3c8642b5c97512c83735143
plosys <89532245+plosys@users.noreply.github.com>
2022-08-19 13:15:45 +0000

    remove bad file

--- packages/utils/validation.ts
+++ packages/utils/validation.ts
@@ -106,4 +106,141 @@ function validateResourceSchemas(definition: AppDefinition, report: Report): voi
               }
               continue;
             }
-            
+            if (validatorKey === 'format') {
+              if (value !== 'date-time') {
+                report(value, 'must be date-time', [
+                  ...prefix,
+                  'properties',
+                  propertyName,
+                  validatorKey,
+                ]);
+              }
+              continue;
+            }
+            report(value, 'does not support custom validators', [
+              ...prefix,
+              'properties',
+              propertyName,
+              validatorKey,
+            ]);
+          }
+        } else if (propertyName.startsWith('$')) {
+          report(propertySchema, 'may not start with $', [...prefix, 'properties', propertyName]);
+        }
+      }
+    }
+  }
+}
+
+function validateBlocks(
+  definition: AppDefinition,
+  blockVersions: Map<string, Map<string, BlockManifest>>,
+  report: Report,
+): void {
+  iterApp(definition, {
+    onBlock(block, path) {
+      const type = normalizeBlockName(block.type);
+      const versions = blockVersions.get(type);
+      if (!versions) {
+        report(block.type, 'is not a known block type', [...path, 'type']);
+        return;
+      }
+      const actionParameters = new Set<string>();
+      const version = versions.get(block.version);
+      if (!version) {
+        report(block.version, 'is not a known version for this block type', [...path, 'version']);
+        return;
+      }
+
+      if (version.parameters) {
+        const validator = new Validator();
+
+        validator.customFormats.fontawesome = () => true;
+        validator.customFormats.remapper = () => true;
+        validator.customFormats.action = (property) => {
+          actionParameters.add(property);
+          return has(block.actions, property);
+        };
+        validator.customFormats['event-listener'] = (property) =>
+          has(block.events?.listen, property);
+        validator.customFormats['event-emitter'] = (property) => has(block.events?.emit, property);
+        const result = validator.validate(block.parameters || {}, version.parameters, {
+          nestedErrors: true,
+        });
+        if ('parameters' in block) {
+          for (const error of result.errors) {
+            report(error.instance, error.message, [...path, 'parameters', ...error.path]);
+          }
+        } else if (!result.valid) {
+          report(block, 'requires property "parameters"', path);
+        }
+      } else if (block.parameters) {
+        report(block.parameters, 'is not allowed on this block type', [...path, 'parameters']);
+      }
+
+      if (block.actions) {
+        if (version.actions) {
+          for (const [key, action] of Object.entries(block.actions)) {
+            if (version.actions.$any) {
+              if (actionParameters.has(key)) {
+                continue;
+              }
+
+              if (!has(version.actions, key) && !version.wildcardActions) {
+                report(action, 'is unused', [...path, 'actions', key]);
+              }
+            } else if (!has(version.actions, key)) {
+              report(action, 'is an unknown action for this block', [...path, 'actions', key]);
+            }
+          }
+        } else {
+          report(block.actions, 'is not allowed on this block', [...path, 'actions']);
+        }
+      }
+
+      if (!block.events) {
+        return;
+      }
+      if (block.events.emit) {
+        for (const [key, value] of Object.entries(block.events.emit)) {
+          if (!version.events?.emit?.$any && !has(version.events?.emit, key)) {
+            report(value, 'is an unknown event emitter', [...path, 'events', 'emit', key]);
+          }
+        }
+      }
+      if (block.events.listen) {
+        for (const [key, value] of Object.entries(block.events.listen)) {
+          if (!version.events?.listen?.$any && !has(version.events?.listen, key)) {
+            report(value, 'is an unknown event listener', [...path, 'events', 'listen', key]);
+          }
+        }
+      }
+    },
+  });
+}
+
+function checkCyclicRoleInheritance(
+  roles: Record<string, RoleDefinition>,
+  name: string,
+  report: Report,
+): void {
+  let lastchecked: string;
+  const stack: string[] = [];
+
+  const checkRoleRecursively = (role: string): boolean => {
+    lastchecked = role;
+    if (stack.includes(role)) {
+      return true;
+    }
+    stack.push(role);
+    return roles[role]?.inherits?.some(checkRoleRecursively);
+  };
+
+  const duplicate = checkRoleRecursively(name);
+  if (duplicate && lastchecked === name) {
+    report(roles[name], 'cyclicly inherits itself', ['security', 'roles', name]);
+  }
+}
+
+/**
+ 
